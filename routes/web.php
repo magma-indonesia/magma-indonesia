0885ab798293e56ea728a32185b30aef9f2c09fc
martanto <martanto@live.com>
2017-11-16 17:33:38 +0700

    Change Routes

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,29 @@
 <?php
 
+Route::get('/', 'HomeController@index')->name('home');
+
+
 /*
+| Magma Indonesia - Routes
 |--------------------------------------------------------------------------
-| Web Routes
+| Users Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| UserConstroller, including View, Login, Register, Update, Delete
 |
 */
+Route::resource('users','UserController');
+Route::get('/login', 'UserController@showLoginForm')->name('login');
+Route::post('/login', 'UserController@login');
+Route::get('/logout', 'UserController@logout')->name('logout');
 
-Route::get('/', function () {
-    return view('welcome');
+/*
+|--------------------------------------------------------------------------
+| Chamber Routes
+|--------------------------------------------------------------------------
+| UserConstroller, including View, Login, Register, Update, Delete
+|
+*/
+Route::get('/chambers', 'ChamberController@index')->name('chamber');
+Route::group(['prefix' => 'chambers'], function (){
+    Route::resource('users', 'UserController');
 });
