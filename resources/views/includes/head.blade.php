1bb24c96ad4c680b98f864e3ebeb734fa42794c4
Martanto <martanto@live.com>
2017-11-19 23:09:58 +0800

    Add Head html blade

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,21 +1,25 @@
 <head>
-    <title>@yield('title')</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta charset="utf-8">
-    <meta content="ie=edge" http-equiv="x-ua-compatible">
-    <meta content="geohazard" name="keywords">
-    <meta content="CVGHM | ESDM" name="author">
-    <meta content="MAGMA Indonesia - Bridging The WIll of Nature to Society" name="description">
-    <meta content="width=device-width, initial-scale=1" name="viewport">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <!-- Page title -->
+    <title>@yield('title')</title>
+
     <link href="{{ asset('favicon.png') }}" rel="shortcut icon">
-    <link href="//fast.fonts.net/cssapi/175a63a1-3f26-476a-ab32-4e21cbdb8be2.css" rel="stylesheet" type="text/css">
-    <link href="{{ asset('bower_components/select2/dist/css/select2.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('bower_components/bootstrap-daterangepicker/daterangepicker.css') }}" rel="stylesheet">
-    <link href="{{ asset('bower_components/dropzone/dist/dropzone.css') }}" rel="stylesheet">
-    <link href="{{ asset('bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('bower_components/fullcalendar/dist/fullcalendar.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('bower_components/perfect-scrollbar/css/perfect-scrollbar.min.css') }}" rel="stylesheet">
+
+    <!-- Vendor styles -->
+    <link rel="stylesheet" href="{{ asset('vendor/fontawesome/css/font-awesome.css') }}" />
+    <link rel="stylesheet" href="{{ asset('vendor/metisMenu/dist/metisMenu.css') }}" />
+    <link rel="stylesheet" href="{{ asset('vendor/animate.css/animate.css') }}" />
+    <link rel="stylesheet" href="{{ asset('vendor/bootstrap/dist/css/bootstrap.css') }}" />
+    @yield('add-vendor-css')
+    <!-- App styles -->
+    <link rel="stylesheet" href="{{ asset('fonts/pe-icon-7-stroke/css/pe-icon-7-stroke.css') }}" />
+    <link rel="stylesheet" href="{{ asset('fonts/pe-icon-7-stroke/css/helper.css') }}" />
+    <link rel="stylesheet" href="{{ asset('styles/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('styles/app.css') }}">
+
     @yield('add-css')
-    <link href="{{ asset('css/main.css?version=2.6') }}" rel="stylesheet">
+
 </head>
