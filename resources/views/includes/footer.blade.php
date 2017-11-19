faf324f175ae4ae986c5e7be600cf13929baf1b5
Martanto <martanto@live.com>
2017-11-19 23:10:04 +0800

    Add footer

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,5 @@
+        <footer class="footer">
+            <span class="pull-right">
+                MAGMA Indonesia © 2015-2017 PVMBG. All rights reserved.
+            </span>
+        </footer>
