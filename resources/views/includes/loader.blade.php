de252e0cba19963702c1e097d3419d597399aa76
Martanto <martanto@live.com>
2017-11-19 23:09:37 +0800

    Add Loader blade

---
+++ resources/views/includes/loader.blade.php
@@ -0,0 +1,16 @@
+	<div class="splash">
+		<div class="color-line"></div>
+		<div class="splash-title">
+			<h1>{{ config('app.name')}}</h1>
+            <h4>Bridging the Will of Nature to Society</h4>
+            <hr>
+			<h2>"Bekerjalah tanpa mengeluh dan bergunalah untuk orang lain"</h2>
+			<div class="spinner">
+				<div class="rect1"></div>
+				<div class="rect2"></div>
+				<div class="rect3"></div>
+				<div class="rect4"></div>
+				<div class="rect5"></div>
+			</div>
+		</div>
+	</div>
