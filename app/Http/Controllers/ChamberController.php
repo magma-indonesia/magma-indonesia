1a8e6bf3f56766d76be5b63d30d5cb01f3bc88aa
Martanto <martanto@live.com>
2017-11-19 20:52:19 +0800

    Add auth middleware to Chamber

--- app/Http/Controllers/ChamberController.php
+++ app/Http/Controllers/ChamberController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 class ChamberController extends Controller
 {
     //
+    public function __construct(Request $request)
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
 
