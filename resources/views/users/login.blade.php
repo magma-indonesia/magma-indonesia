7df9607bb31c30db710d2ea982f82a837d4013ab
Martanto <martanto@live.com>
2017-11-19 23:06:48 +0800

    Fixing CSS asset

--- resources/views/users/login.blade.php
+++ resources/views/users/login.blade.php
@@ -2,65 +2,89 @@
 <html lang="{{ app()->getLocale() }}">
 
 <head>
-    <title>Login</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta charset="utf-8">
-    <meta content="ie=edge" http-equiv="x-ua-compatible">
-    <meta content="geohazard" name="keywords">
-    <meta content="CVGHM | ESDM" name="author">
-    <meta content="MAGMA Indonesia Login Form" name="description">
-    <meta content="width=device-width, initial-scale=1" name="viewport">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link href="favicon.png" rel="shortcut icon">
-    <link href="//fast.fonts.net/cssapi/175a63a1-3f26-476a-ab32-4e21cbdb8be2.css" rel="stylesheet" type="text/css">
-    <link href="{{ asset('css/main.css?version=2.6') }}" rel="stylesheet">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+
+	<!-- Page title -->
+	<title>Login</title>
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+    <link href="{{ asset('favicon.png') }}" rel="shortcut icon">
+
+	<!-- Vendor styles -->
+	<link rel="stylesheet" href="{{ asset('vendor/bootstrap/dist/css/bootstrap.css') }}" />
+
+	<!-- App styles -->
+	<link rel="stylesheet" href="{{ asset('styles/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('styles/app.css') }}">
+
 </head>
 
-<body class="auth-wrapper">
-    <div class="all-wrapper menu-side with-pattern">
-        <div class="auth-box-w">
-            <div class="logo-w">
-                <a href="index.html">
-                    <img alt="" src="img/logo-big.png">
-                </a>
-            </div>
-            <h4 class="auth-header">
-                Login Form
-            </h4>
-            <form method="POST" action="{{ route('login') }}">
-                {{ csrf_field() }}
-                <div class="form-group">
-                    <label for="">NIP / Email</label>
-                    <input  name="username" class="form-control" placeholder="Masukkan NIP/Email" type="text" required>
-                    <div class="pre-icon os-icon os-icon-user-male-circle"></div>
-                </div>
-                <div class="form-group">
-                    <label for="">Password</label>
-                    <input  name="password" class="form-control" placeholder="Password" type="password" required>
-                    <div class="pre-icon os-icon os-icon-fingerprint"></div>
-                </div>
+<body class="blank">
 
-            @if (count($errors) > 0)
-                <div class="form-group {{ count($errors) > 0 ? 'has-error has-danger' : '' }}">
-                    <div class="help-block form-text with-errors form-control-feedback">
-                        <ul class="list-unstyled">
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                </div>
-            @endif
-                <div class="buttons-w">
-                    <button type="submit" class="btn btn-primary">Login MAGMA</button>
-                    {{--  <div class="form-check-inline">
-                        <label class="form-check-label">
-                            <input class="form-check-input" type="checkbox">Remember Me</label>
-                    </div>  --}}
-                </div>
-            </form>
-        </div>
-    </div>
+    @include('includes.loader')
+
+	<div class="color-line"></div>
+
+	<div class="login-container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="text-center m-b-md">
+					<h2>Login</h2>
+				</div>
+				<div class="hpanel">
+					<div class="panel-body">
+						<form method="POST" action="{{ route('login') }}" id="loginForm">
+                            {{ csrf_field() }}
+							<div class="form-group">
+								<label class="control-label" for="username">NIP/Email</label>
+								<input type="text" placeholder="Masukkan NIP atau email Anda" title="NIP/Email" name="username" id="username" class="form-control"
+								 required>
+							</div>
+							<div class="form-group">
+								<label class="control-label" for="password">Password</label>
+								<input type="password" title="Password" placeholder="******" name="password" id="password"
+								 class="form-control" required>
+							</div>
+
+							@if(count($errors) > 0)
+							<div class="form-group">
+                                <div class="alert alert-danger">
+									<ul class="list-unstyled">
+										@foreach ($errors->all() as $error)
+											<li>{{ $error }}</li>
+										@endforeach
+									</ul>
+                                </div>
+							</div>
+							@endif
+
+							<button type="submit" class="btn btn-success btn-block">Login</button>
+							<a class="btn btn-default btn-block" href="{{ route('users.create') }}">Register</a>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12 text-center">
+				<strong>PVMBG</strong> - MAGMA Indonesia
+				<br/> 2015 Copyright Kementerian Energi dan Sumber Daya Mineral
+			</div>
+		</div>
+	</div>
+
+
+	<!-- Vendor scripts -->
+	<script src="{{ asset('vendor/jquery/dist/jquery.min.js') }}"></script>
+	<script src="{{ asset('vendor/jquery-ui/jquery-ui.min.js') }}"></script>
+	<script src="{{ asset('vendor/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+
+	<!-- App scripts -->
+	<script src="{{ asset('scripts/login.js') }}"></script>
+
 </body>
 
 </html>
