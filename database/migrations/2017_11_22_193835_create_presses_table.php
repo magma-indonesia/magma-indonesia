363ca52aadd2d5ffd4c7a61286ff6dfe172e7af9
Martanto <martanto@live.com>
2017-12-06 06:55:29 +0700

    Add Press Release Migration

---
+++ database/migrations/2017_11_22_193835_create_presses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('presses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');         
+            $table->text('body');
+            $table->integer('view_count')->default(0);
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('presses');
+    }
+}
