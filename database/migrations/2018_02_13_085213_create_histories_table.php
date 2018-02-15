8abc29f63711cea52508ea0c7fdbe5d0e2973093
Martanto <martanto@live.com>
2018-02-15 11:23:52 +0700

    Create History migration table

---
+++ database/migrations/2018_02_13_085213_create_histories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHistoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('histories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('code_id',3)->unique();
+            $table->foreign('code_id')->references('code')->on('ga_dd');
+            $table->text('body');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('histories');
+    }
+}
