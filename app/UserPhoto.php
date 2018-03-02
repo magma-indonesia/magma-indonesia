6777b784a96116ac47cff366a7adbc70b6536f8a
Martanto <martanto@live.com>
2018-03-02 09:00:06 +0700

    Add table

--- app/UserPhoto.php
+++ app/UserPhoto.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserPhoto extends Model
 {
+    protected $table    = 'users_photo';
     /**
      * The attributes that are mass assignable.
      *
