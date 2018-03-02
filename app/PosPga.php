43e1cc6172c563f887fcfd93a0c198414bd37e7f
Martanto <martanto@live.com>
2018-03-02 08:59:56 +0700

    Add Gadd and Pos Relationship

--- app/PosPga.php
+++ app/PosPga.php
@@ -19,4 +19,16 @@ class PosPga extends Model
         'updated_at'
 
     ];
+
+    /**     
+     *   Masing-masing Pos hanya dimiliki
+     *   oleh 1 Gunungapi
+     * 
+     *   @return \App\Gadd
+     * 
+     */
+    public function gunungapi()
+    {
+        return $this->belongsTo('App\Gadd','code_id','code');
+    }
 }
