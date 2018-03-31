63eafc546af3aeb77aa0328abdb0059510146a71
Martanto <martanto@live.com>
2018-03-31 16:45:31 +0700

    Update UserCollection

--- app/Http/Resources/UserCollection.php
+++ app/Http/Resources/UserCollection.php
@@ -14,6 +14,6 @@ class UserCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return UserResource::collection($this->collection);
     }
 }
