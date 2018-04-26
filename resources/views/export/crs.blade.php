b66a9bd486286727304572d1ddb9ba0e73376a49
Martanto <martanto@live.com>
2018-04-26 17:01:26 +0700

    Remove useless class

--- resources/views/export/crs.blade.php
+++ resources/views/export/crs.blade.php
@@ -1,4 +1,4 @@
-<table class="table table-striped">
+<table>
     <thead>
         <tr>
             <th>Status</th>
@@ -9,21 +9,25 @@
             <th>Validator</th>
             <th>Waktu Kejadian</th>
             <th>Lokasi</th>
+            <th>Latitude</th>
+            <th>Longitude</th>
             <th>Keterangan</th>
         </tr>
     </thead>
     <tbody>
         @foreach($crs as $item)
         <tr title="{{ $item->name }}">
-            <td class="{{ $item->status == 'BARU' ? 4 : ($item->status == 'DRAFT' ? 2 : 1) }}">{{ title_case($item->status) }}</td>
+            <td>{{ title_case($item->status) }}</td>
             <td>{{ $item->name }}</td>
             <td>{{ $item->phone }}</td>
             <td>{{ title_case($item->type) }}</td>
-            <td><span class="label {{ optional($item->validator)->valid ? $item->validator->valid == 'Valid' ? 'label-success' : 'label-warning': 'label-danger'}}">{{ optional($item->validator)->valid ? $item->validator->valid : 'Belum divalidasi'}}</span></td>
+            <td>{{ optional($item->validator)->valid ? $item->validator->valid : 'Belum divalidasi'}}</td>
             <td>{{ optional($item->validator)->user ? $item->validator->user->name : '-'}}</td>  
             <td>{{ $item->waktu_kejadian }}</td>
             <td>{{ Indonesia::findProvince($item->province_id)->name .', '. Indonesia::findCity($item->city_id)->name .', '. Indonesia::findDistrict($item->district_id)->name }}</td>
-            <td>{{ $item->tsc .' - '. $item->ksc }}</td>                                      
+            <td>{{ $item->latitude }}</td>
+            <td>{{ $item->longitude }}</td>
+            <td>{{ $item->tsc .', '. $item->ksc }}</td>                                      
         </tr>
         @endforeach
     </tbody>
