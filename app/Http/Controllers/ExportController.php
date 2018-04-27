d5f7e09ee9d9f5339d775f2a2fe398d5a3872476
Martanto <martanto@live.com>
2018-04-27 15:42:05 +0700

    Update Var Export

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -5,12 +5,34 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
-use App\Export\CrsExport;
+use App\Export\VarExport;
 
 class ExportController extends Controller
 {
-    public function crs(Request $request)
+
+    public function index(Request $request, $type)
+    {
+        switch ($type) {
+            case 'crs':
+                return $this->crs($request);
+                break;
+            case 'var':
+                return $this->var($request);
+                break;
+            default:
+                # code...
+                break;
+        }
+        
+    }
+
+    public function crs($request)
     {
         return (new CrsExport($request))->download('MAGMA_crs_'.now().'.xlsx');
+    }
+
+    public function var($request)
+    {
+        return (new VarExport($request))->download('MAGMA_var_'.now().'.xlsx');
     }
 }
