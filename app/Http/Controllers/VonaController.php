f19b13321b16d8b911dfd96c1de18a4527587d73
Martanto <martanto@live.com>
2018-05-13 20:00:44 +0700

    Update VONA Controller

--- app/Http/Controllers/VonaController.php
+++ app/Http/Controllers/VonaController.php
@@ -3,18 +3,51 @@
 namespace App\Http\Controllers;
 
 use App\Vona;
+use App\Gadd;
+use App\User;
 use Illuminate\Http\Request;
 
 class VonaController extends Controller
 {
+    protected function location($type,$decimal)
+    {
+        $vars = explode(".",$decimal);
+        $deg = $vars[0];
+        $tempma = "0.".$vars[1];
+        $tempma = $tempma * 3600;
+        $min = floor($tempma / 60);
+        $sec = round($tempma - ($min*60));
+
+        $sym = "N ";
+        if ($deg<10 AND $deg>0){
+            $deg="0".$deg;
+        } else {
+            if ($deg<0){
+                $deg="0".abs($deg);
+                $sym="S ";
+            }
+        }
+        if ($min<10){
+            $min="0".$min;
+        }
+        if ($sec<10){
+            $sec="0".$sec;
+        }
+
+        if ($type == 'lat')
+        {
+            return $sym.$deg.' deg '.$min.' min '.$sec.' sec '; 
+        }
+        return 'E '.$deg.' deg '.$min.' min '.$sec.' sec'; 
+    }
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        $vonas = Vona::orderBy('issued','desc')->paginate(10);
+    public function index(Request $request)
+    {        
+        $vonas = Vona::orderBy('issued','desc')->paginate(30);
 
         return view('vona.index',compact('vonas'));
     }
@@ -26,7 +59,13 @@ class VonaController extends Controller
      */
     public function create()
     {
-        //
+        $gadds = Gadd::select('name','code')->orderBy('name')->get();
+
+        $users = User::whereHas('bidang', function($query){
+            $query->where('user_bidang_desc_id','like',2);
+        })->orderBy('name')->get();
+        
+        return view('vona.create',compact('gadds','users'));
     }
 
     /**
@@ -37,7 +76,53 @@ class VonaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $nip = $request->input('nip',auth()->user()->nip);
+
+        $request->merge([
+            'nip' => $nip,
+        ]);
+
+        $this->validate($request, [
+            'nip' => 'required|digits_between:16,18',
+            'code' => 'required|size:3',
+            'tipe' => 'required|in:real,exercise',
+            'cucode' => 'required|in:green,yellow,orange,red',
+            'vas' => 'required|min:50',
+            'vch' => 'required|max:5',
+            'ovci' => 'required|min:20',
+            'remarks' => 'nullable'
+        ]);
+
+        $user = User::where('nip','like',$nip)->firstOrFail();
+        $gadd = Gadd::where('code',$request->code)->firstOrFail();
+
+        $location = $this->location('lat',$gadd->latitude).$this->location('lon',$gadd->longitude);
+
+        $issued = now('UTC')->toDateTimeString();
+        $tipe = strtoupper($request->input('tipe','real'));
+        $code = $request->code;
+        $cucode = strtoupper($request->cucode);
+        $vas = $request->vas;
+        $vch = $request->vch;
+        $ovci = $request->ovci;
+        $remarks = $request->remarks;
+
+        $vona = Vona::create([
+            'issued' => $issued,
+            'type' => $tipe,
+            'code_id' => $code,
+            'cu_code' => $cucode,
+            'location' => $location,
+            'vas' => $vas,
+            'vch_summit' => $vch,
+            'vch_asl' => $vch+$gadd->elevation,
+            'vch_other' => $ovci,
+            'remarks' => $remarks,
+            'nip_pelapor' => $nip
+        ]);
+
+        return view('vona.show',compact('vona'));
+
     }
 
     /**
@@ -84,4 +169,5 @@ class VonaController extends Controller
     {
         //
     }
+
 }
