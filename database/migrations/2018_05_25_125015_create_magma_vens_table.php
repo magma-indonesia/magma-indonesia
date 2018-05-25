4656036533889348cafec894da657c3f824fdd94
Martanto <martanto@live.com>
2018-05-25 15:11:14 +0700

    Create MagmaVen table

---
+++ database/migrations/2018_05_25_125015_create_magma_vens_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMagmaVensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('magma_vens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->uuid('uuid')->unique();
+            $table->char('code_id',3)->index();
+            $table->foreign('code_id')->references('code')->on('ga_dd');
+            $table->date('date');
+            $table->time('time');
+            $table->boolean('visibility')->default(1);
+            $table->integer('height')->default(0);
+            $table->char('wasap',255);
+            $table->char('arah_asap',100);
+            $table->float('amplitudo',8,2)->default(0);
+            $table->float('durasi',8,2)->default(0);
+            $table->string('photo')->nullable();
+            $table->tinyInteger('status');
+            $table->text('rekomendasi')->nullable();
+            $table->text('lainnya')->nullable();
+            $table->char('nip_pelapor',18);
+            $table->foreign('nip_pelapor')->references('nip')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('magma_vens');
+    }
+}
