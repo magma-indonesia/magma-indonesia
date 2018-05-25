49e1f37c2e92d266dc309ebb1dc6195a779fd7ca
Martanto <martanto@live.com>
2018-05-25 15:09:08 +0700

    FIx Header response

--- app/Http/Middleware/RevalidateBackHistory.php
+++ app/Http/Middleware/RevalidateBackHistory.php
@@ -16,9 +16,12 @@ class RevalidateBackHistory
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        return $response->header('Cache-Control','nocache, no-store, max-age=0, must-revalidate')
-            ->header('Pragma','no-cache')
-            ->header('Expires','Fri, 01 Jan 1990 00:00:00 GMT');
+        
+        $response->headers->set('Cache-Control','nocache, no-store, max-age=0, must-revalidate');
+        $response->headers->set('Pragma','no-cache');
+        $response->headers->set('Expires','Fri, 01 Jan 1990 00:00:00 GMT');
+
+        return $response;
         
     }
 }
