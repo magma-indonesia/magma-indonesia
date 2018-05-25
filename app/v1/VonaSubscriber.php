80d1e1d3c76ce5b4cbc411582640106e1b3560b5
Martanto <martanto@live.com>
2018-05-25 15:09:54 +0700

    Old magma_subscribe

---
+++ app/v1/VonaSubscriber.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\v1;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VonaSubscriber extends Model
+{
+    protected $connection = 'magma';
+
+    protected $table = 'magma_subscribe';
+
+    protected $primaryKey = 'no';
+}
