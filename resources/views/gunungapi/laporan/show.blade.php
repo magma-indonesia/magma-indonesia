2e66cd0a969abf0e6be44bcd501e1c3d2b1fe786
Martanto <martanto@live.com>
2018-05-31 10:32:09 +0700

    Update show var

--- resources/views/gunungapi/laporan/show.blade.php
+++ resources/views/gunungapi/laporan/show.blade.php
@@ -75,6 +75,132 @@
                     <i class="fa fa-eye"> </i> 142 views
                 </div>
             </div>
+            <div class="hpanel">
+                <div class="panel-heading hbuilt">
+                    <div class="p-xs h4">
+                        <small class="pull-right">
+                        @switch($var->statuses_desc_id)
+                            @case(1)
+                            <span class="label label-normal">{{ $var->status->status }}</span>
+                            @break
+                            @case(2)
+                            <span class="label label-waspada">{{ $var->status->status }}</span>
+                            @break
+                            @case(3)
+                            <span class="label label-siaga">{{ $var->status->status }}</span>
+                            @break
+                            @default
+                            <span class="label label-awas">{{ $var->status->status }}</span>
+                        @endswitch
+                        </small>
+                        Laporan Aktivitas Gunung Api {{ $var->gunungapi->name }}
+                    </div>
+                </div>
+                <div class="border-top border-left border-right bg-light">
+                    <div class="row">
+                        <div class="col-sm-2 hidden-xs">
+                            <div class="profile-picture">
+                                <img alt="logo" class="img-circle p-m" src="{{ route('user.photo',['id' => $var->user->id]) }}">
+                            </div>
+                        </div>
+                        <div class="col-sm-10">
+                            <div class="p-m">
+                                <div>
+                                    <span class="font-extra-bold">Pengirim: </span>
+                                    <a href="#">{{ $var->user->name }}</a>
+                                </div>
+                                <div>
+                                    <span class="font-extra-bold">Pos: </span>
+                                    <a href="#">{{ $var->pos->observatory }}</a>
+                                </div>
+                                <div>
+                                    <span class="font-extra-bold">Tanggal: </span>
+                                    {{ $var->var_data_date->format('Y-m-d')}}
+                                </div>
+                                <div>
+                                    <span class="font-extra-bold">Periode: </span>
+                                    {{ $var->var_perwkt.' Jam, Pukul '.$var->periode }}
+                                </div>
+                                <div>
+                                    <span class="font-extra-bold">Validasi Penanggung Jawab: </span>
+                                    {{ $var->pj->nip_id ?? 'Belum divalidasi' }}
+                                </div>
+                                <div>
+                                    <span class="font-extra-bold">Verifikator: </span>
+                                    {{ $var->verifikator->nip_id ?? 'Belum diverifikasi' }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-lg-6 border-right">
+                            <h4>Pengamatan Visual </h4>
+                            <p>{!! $visual !!}</p>
+                            <h4>Keterangan Visual Lainnya </h4>
+                            <p>{{ $var->visual->visual_kawah }}</p>
+                        </div>
+                        <div class="col-lg-6">
+                            <h4>Kegempaan </h4>
+                            <p>{{ $gempa }}</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="border-bottom border-left border-right bg-white p-m">
+                    <p class="m-b-md">
+                        <span><i class="fa fa-paperclip"></i> 3 attachments - </span>
+                        <a href="#" class="btn btn-default btn-xs">Download all in zip format <i class="fa fa-file-zip-o"></i> </a>
+                    </p>
+
+                    <div>
+                        <div class="row">
+                            <div class="col-sm-3">
+                                <div class="hpanel">
+                                    <div class="panel-body file-body">
+                                        <i class="fa fa-file-pdf-o text-info"></i>
+                                    </div>
+                                    <div class="panel-footer">
+                                        <a href="#">Document_2016.doc</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-3">
+                                <div class="hpanel">
+                                    <div class="panel-body file-body">
+                                        <i class="fa fa-file-audio-o text-warning"></i>
+                                    </div>
+                                    <div class="panel-footer">
+                                        <a href="#">Audio_2016.doc</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-3">
+                                <div class="hpanel">
+                                    <div class="panel-body file-body">
+                                        <i class="fa fa-file-excel-o text-success"></i>
+                                    </div>
+                                    <div class="panel-footer">
+                                        <a href="#">Sheets_2016.doc</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+                    </div>
+                </div>
+
+                <div class="panel-footer text-right">
+                    <div class="btn-group">
+                        <button class="btn btn-default"><i class="fa fa-reply"></i> Reply</button>
+                        <button class="btn btn-default"><i class="fa fa-arrow-right"></i> Forward</button>
+                        <button class="btn btn-default"><i class="fa fa-print"></i> Print</button>
+                        <button class="btn btn-default"><i class="fa fa-trash-o"></i> Remove</button>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
