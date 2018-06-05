e402be5d7897c0d9a54ae45511a2dbcd0a7822c7
Martanto <martanto@live.com>
2018-06-05 08:21:58 +0700

    Update show letusan

--- resources/views/gunungapi/letusan/show.blade.php
+++ resources/views/gunungapi/letusan/show.blade.php
@@ -18,14 +18,14 @@
                             <span>Laporan Letusan</span>
                         </li>
                         <li class="active">
-                            <span>Buat Laporan Letusan</span>
+                            <span>{{ $ven->gunungapi->name }}</span>
                         </li>
                     </ol>
                 </div>
                 <h2 class="font-light m-b-xs">
-                    Buat Laporan Letusan Gunung Api
+                    Laporan Letusan Gunung Api {{ $ven->gunungapi->name }}
                 </h2>
-                <small>Form input letusan Gunung Api</small>
+                <small>Letusan Gunung Api</small>
             </div>
         </div>
     </div>
@@ -41,9 +41,13 @@
                                 <img alt="logo" class="p-m" src="{{ url('/').'/img/volcano.svg' }}" style="width: 180px;">
                             </div>
                         <h3><b>Informasi Erupsi G. {{ $ven->gunungapi->name }}</b></h3>
-                        <p>Telah terjadi erupsi G. {{ $ven->gunungapi->name }}, {{ $ven->gunungapi->province }} pada {{ $ven->date }}, pukul {{ $ven->time.$ven->gunungapi->zonearea}} dengan tinggi kolom abu teramati &plusmn; {{ $ven->height }} m di atas puncak (&plusmn; {{ $ven->height+$ven->gunungapi->elevation }} m di atas permukaan laut). Kolom abu teramati berwarna {{ count($ven->wasap) > 1 ? strtolower($ven->wasap[0]).' hingga '.strtolower(last($ven->wasap)) : strtolower($ven->wasap[0]) }} dengan intensitas sedang hingga kuat condong ke arah  {{ count($ven->arah_asap) > 1 ? strtolower($ven->arah_asap[0]).' dan '.strtolower(last($ven->arah_asap)) : strtolower($ven->arah_asap[0]) }}. {{ $ven->amplitudo > 0 ? 'Erupsi ini terekam di seismograf dengan amplitudo maksimum '.$ven->amplitudo.' mm dan durasi '.$ven->durasi.' detik.' : ''}}</p>
+                        <blockquote class="pull-left">
+                                <p>Telah terjadi erupsi G. {{ $ven->gunungapi->name }}, {{ $ven->gunungapi->province }} pada {{ $ven->date }}, pukul {{ $ven->time.$ven->gunungapi->zonearea}} dengan tinggi kolom abu teramati &plusmn; {{ $ven->height }} m di atas puncak (&plusmn; {{ $ven->height+$ven->gunungapi->elevation }} m di atas permukaan laut). Kolom abu teramati berwarna {{ count($ven->wasap) > 1 ? strtolower($ven->wasap[0]).' hingga '.strtolower(last($ven->wasap)) : strtolower($ven->wasap[0]) }} dengan intensitas sedang hingga kuat condong ke arah  {{ count($ven->arah_asap) > 1 ? strtolower($ven->arah_asap[0]).' dan '.strtolower(last($ven->arah_asap)) : strtolower($ven->arah_asap[0]) }}. {{ $ven->amplitudo > 0 ? 'Erupsi ini terekam di seismograf dengan amplitudo maksimum '.$ven->amplitudo.' mm dan durasi '.$ven->durasi.' detik.' : ''}}</p>
+                        </blockquote>
                         <h3><b>Rekomendasi</b></h3>
-                        <p>{!! nl2br($ven->rekomendasi) !!}</p>
+                        <blockquote class="pull-left">
+                            <p>{!! nl2br($ven->rekomendasi) !!}</p>
+                        </blockquote>
                     </div>
                     <div class="panel-footer">
                         <span class="pull-right">
