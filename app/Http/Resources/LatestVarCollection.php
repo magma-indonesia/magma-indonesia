1050df94ed716cdfb08084fc1f713386da64a4fa
Martanto <martanto@live.com>
2018-06-06 09:39:53 +0700

    Add meta key

--- app/Http/Resources/LatestVarCollection.php
+++ app/Http/Resources/LatestVarCollection.php
@@ -7,6 +7,21 @@ use Illuminate\Http\Resources\Json\ResourceCollection;
 class LatestVarCollection extends ResourceCollection
 {
     /**
+     * Get additional data that should be returned with the resource array.
+     *
+     * @param \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function with($request)
+    {
+        return [
+            'meta' => [
+                'key' => 'value'
+            ],
+        ];
+    }
+    
+    /**
      * Transform the resource collection into an array.
      *
      * @param  \Illuminate\Http\Request  $request
