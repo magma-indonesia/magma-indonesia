ba2b31ec85670605e9291f49a1cada61501a61c9
Martanto <martanto@live.com>
2018-06-11 20:57:07 +0700

    Initiate UserAdministratif Model

--- app/UserAdministratif.php
+++ app/UserAdministratif.php
@@ -13,14 +13,46 @@ class UserAdministratif extends Model
      */
     protected $fillable = [
         'user_id',
-        'bidang_id',
+        'bidang',
+        'tanggal_lahir',
+        'tempat_lahir',
+        'agama',
+        'kelamin',
+        'status_nikah',
         'jabatan',
-        'fungsional',
+        'pangkat',
         'golongan',
-        'pangkat'
+        'pendidikan_terakhir',
+        'kantor'
     ];
 
     protected $guarded  = [
         'id'
     ];
+
+    public function getBidangAttribute($value)
+    {
+        switch ($value) {
+            case 1:
+                $bidang = 'Pusat Vulkanologi dan Mitigasi Bencana Geologi';
+                break;
+            case 2:
+                $bidang = 'Mitigasi Gunung Api';
+                break;
+            case 3:
+                $bidang = 'Mitigasi Gerakan Tanah';
+                break;
+            case 4:
+                $bidang = 'Mitigasi Gempa Bumi dan Tsunami';
+                break;
+            case 5:
+                $bidang = 'Balai Penyelidikan dan Pengembangan Teknologi Kebencanaan Geologi';
+                break;
+            default:
+                $bidang = 'Tata Usaha';
+                break;
+        }
+
+        return $bidang;
+    }
 }
