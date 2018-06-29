598859c529d4dfd27ea1b6ee9f34a0e1da8a9190
Martanto <martanto@live.com>
2018-06-29 08:42:18 +0700

    Update comment

--- app/SigertanCrs.php
+++ app/SigertanCrs.php
@@ -7,9 +7,18 @@ use Indonesia;
 
 class SigertanCrs extends Model
 {
-   
+   /**
+    * Casting date attribute untuk beberapa kolom
+    *
+    * @var array
+    */
     protected $dates = ['waktu_kejadian','tsc'];
 
+    /**
+     * Fillable columns
+     *
+     * @var array
+     */
     protected $fillable = [
         'name',
         'phone',
@@ -33,6 +42,11 @@ class SigertanCrs extends Model
         'longitude_user'
     ];
 
+    /**
+     * Hide from json array
+     *
+     * @var array
+     */
     protected $hidden = [
         'id',
     ];
@@ -41,29 +55,55 @@ class SigertanCrs extends Model
         'id'
     ];
 
+    /**
+     * Menambahkan array dalam json
+     *
+     * @var array
+     */
     protected $appends = ['provinsi','kota','kecamatan'];
 
+    /**
+     * Mutator untuk Tyoe attribute
+     *
+     * @param [type] $value
+     * @return void
+     */
     public function getTypeAttribute($value)
     {
         return title_case($value);
     }
 
+    /**
+     * Mutator untuk attribute provinsi
+     *
+     * @return void
+     */
     public function getProvinsiAttribute()
     {
         return Indonesia::findProvince($this->province_id)->name;
     }
 
+    /**
+     * Mutator untuk attribute Kota
+     *
+     * @return void
+     */
     public function getKotaAttribute()
     {
         return Indonesia::findCity($this->city_id)->name;
     }
 
+    /**
+     * Mutator untuk attribute Kecamatan
+     *
+     * @return void
+     */
     public function getKecamatanAttribute()
     {
         return Indonesia::findDistrict($this->district_id)->name;
     }
 
-   /**     
+    /**     
      *   Masing-masing crs hanya memiliki 
      *   1 device yang digunakan untuk membuat laporan
      */
@@ -72,7 +112,7 @@ class SigertanCrs extends Model
         return $this->hasOne('App\SigertanCrsDevices','crs_id','id');
     }
 
-   /**     
+    /**     
      *   Masing-masing crs hanya memiliki 
      *   1 photo laporan
      */
@@ -81,7 +121,7 @@ class SigertanCrs extends Model
         return $this->hasOne('App\SigertanCrsPhotos','crs_id','id');
     }
 
-   /**     
+    /**     
      *   Masing-masing crs hanya bisa
      *  divalidasi oleh 1 user Magma - Struktural
      */
