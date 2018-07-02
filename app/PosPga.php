cc3deeb6aebb4b9155962c121962649e9f4fb4bd
Martanto <martanto@live.com>
2018-07-02 16:10:20 +0700

    Change Pos Pga Model
    - add App\Kantor relationship

--- app/PosPga.php
+++ app/PosPga.php
@@ -10,13 +10,41 @@ class PosPga extends Model
     protected $fillable = [
         'code_id',
         'obscode',
+        'keterangan',
+    ];
+
+    protected $appends = [
         'observatory',
         'address',
         'elevation',
         'latitude',
         'longitude',
-        'keterangan',
     ];
+
+    public function getObservatoryAttribute()
+    {
+        return $this->kantor->nama;
+    }
+
+    public function getAddressAttribute()
+    {
+        return $this->kantor->address;
+    }
+
+    public function getElevationAttribute()
+    {
+        return $this->kantor->elevation;
+    }
+
+    public function getLatitudeAttribute()
+    {
+        return $this->kantor->latitude;
+    }
+
+    public function getLongitudeAttribute()
+    {
+        return $this->kantor->longitude;
+    }
 
     /**     
      *   Masing-masing Pos hanya dimiliki
@@ -28,5 +56,17 @@ class PosPga extends Model
     public function gunungapi()
     {
         return $this->belongsTo('App\Gadd','code_id','code');
+    }
+
+    /**     
+     *   Masing-masing Pos hanya dimiliki
+     *   oleh 1 Gunungapi
+     * 
+     *   @return \App\Gadd
+     * 
+     */
+    public function kantor()
+    {
+        return $this->belongsTo('App\Kantor','obscode','code');
     }
 }
