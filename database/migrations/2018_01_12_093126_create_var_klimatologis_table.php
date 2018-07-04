1c8d9b804c88dae6549a11320e2c87e79da2500c
Martanto <martanto@live.com>
2018-07-04 19:14:49 +0700

    Update var_klimatologi table
    - change value for suhu, kelembaban, tekanan dan curah_hujan

--- database/migrations/2018_01_12_093126_create_var_klimatologis_table.php
+++ database/migrations/2018_01_12_093126_create_var_klimatologis_table.php
@@ -18,15 +18,15 @@ class CreateVarKlimatologisTable extends Migration
             $table->char('noticenumber_id',16)->unique()->index();
             $table->foreign('noticenumber_id')->references('noticenumber')->on('magma_vars')->onDelete('cascade');
             $table->char('cuaca',50)->nullable();
-            $table->decimal('curah_hujan',3,2)->default(0.0);
+            $table->decimal('curah_hujan',5,2)->default(0.0);
             $table->char('kecangin',32)->nullable();
             $table->char('arahangin',100)->nullable();
-            $table->decimal('suhumin',3,2)->default(0.0);
-            $table->decimal('suhumax',3,2)->default(0.0);
-            $table->decimal('lembabmin',3,2)->default(0.0);
-            $table->decimal('lembabmax',3,2)->default(0.0);
-            $table->decimal('tekmin',4,2)->default(0.0);
-            $table->decimal('tekmax',4,2)->default(0.0);
+            $table->decimal('suhumin',4,2)->default(0.0);
+            $table->decimal('suhumax',4,2)->default(0.0);
+            $table->decimal('lembabmin',5,2)->default(0.0);
+            $table->decimal('lembabmax',5,2)->default(0.0);
+            $table->decimal('tekmin',5,2)->default(0.0);
+            $table->decimal('tekmax',5,2)->default(0.0);
             $table->timestamps();
         });
     }
