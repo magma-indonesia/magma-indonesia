022bc5ad9b3f9e05773bc046669ff36ff290727e
Martanto <martanto@live.com>
2018-07-04 19:14:09 +0700

    Update var_visual table
    - change visual_asap to enum

--- database/migrations/2018_01_11_101751_create_var_visuals_table.php
+++ database/migrations/2018_01_11_101751_create_var_visuals_table.php
@@ -18,7 +18,7 @@ class CreateVarVisualsTable extends Migration
             $table->char('noticenumber_id',16)->unique()->index();
             $table->foreign('noticenumber_id')->references('noticenumber')->on('magma_vars')->onDelete('cascade');
             $table->char('visibility',60)->nullable();
-            $table->char('visual_asap',15)->nullable();
+            $table->enum('visual_asap',['Nihil','Teramati','Tidak Teramati']);
             $table->text('visual_kawah')->nullable();
             $table->timestamps();
         });
