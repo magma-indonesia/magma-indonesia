b81a23c22e9baadb1fdf6a508d7c86879e4f2492
Martanto <martanto@live.com>
2018-07-05 08:52:07 +0700

    Add mutator fo OldVen

--- app/v1/MagmaVen.php
+++ app/v1/MagmaVen.php
@@ -11,4 +11,90 @@ class MagmaVen extends Model
     protected $table = 'magma_erupt';
 
     protected $primaryKey = 'erupt_id';
+
+    /**
+     * Merubah status dari string menjadi integer
+     *
+     * @param string $value
+     * @return integer
+     */
+    protected function getStatus(string $value) : int
+    {
+        switch ($value) {
+            case 'Level IV (Awas)':
+                return 4;
+            case 'Level III (Siaga)':
+                return 3;
+            case 'Level II (Waspada)':
+                return 2;
+            default:
+                return 1;
+        }
+    }
+
+    /**
+     * Merubah warna abu dari string menjadi array
+     *
+     * @param string $value
+     * @return array
+     */
+    public function getEruptWrnAttribute(string $value)
+    {
+        return $value == '-' ? null : explode(', ',$value);
+    }
+
+    /**
+     * Merubah intensitas abu dari string menjadi array
+     *
+     * @param string $value
+     * @return array
+     */
+    public function getEruptIntAttribute(string $value)
+    {
+        return $value == '-' ? null : explode(', ',$value);
+    }
+
+    /**
+     * Merubah arah asap dari string menjadi array
+     *
+     * @param string $value
+     * @return array
+     */
+    public function getEruptArhAttribute(string $value)
+    {
+        return $value == '-' ? null : explode(', ',$value);
+    }
+
+    /**
+     * Foto letusan
+     *
+     * @param string $value
+     * @return array
+     */
+    public function getEruptPhtAttribute(string $value)
+    {
+        return $value == '-' ? null : $value;
+    }
+
+    /**
+     * Merubah status string menjadi int
+     *
+     * @param string $value
+     * @return array
+     */
+    public function getEruptStaAttribute(string $value)
+    {
+        return $this->getStatus($value);
+    }
+
+    /**
+     * Merubah nilai empty keterangan menjadi null
+     *
+     * @param string $value
+     * @return array
+     */
+    public function getEruptKetAttribute(string $value)
+    {
+        return $value == '-' ? null : $value;
+    }
 }
