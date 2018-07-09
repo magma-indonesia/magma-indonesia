3594ee6f31e88858472798046e2ce4d89a52c7bd
Martanto <martanto@live.com>
2018-07-09 17:54:35 +0700

    Add each status model (normal, waspada, siaga, awas) as relationship

--- app/v1/Gadd.php
+++ app/v1/Gadd.php
@@ -14,11 +14,35 @@ class Gadd extends Model
 
     protected $table = 'ga_dd';
 
-    public function rekomendasi()
+    public function normal()
     {
-        return $this->hasMany('App\v1\MagmaVar','ga_code','ga_code')
-            ->select('ga_code','cu_status','var_data_date','var_rekom')
-            ->groupBy('ga_code','cu_status')
-            ->orderBy('var_data_date','desc');
+        return $this->hasOne('App\v1\MagmaVar','ga_code','ga_code')
+                    ->select('ga_code','cu_status','var_data_date','var_rekom')
+                    ->where('cu_status','Level I (Normal)')
+                    ->orderBy('var_data_date','desc');
+    }
+
+    public function waspada()
+    {
+        return $this->hasOne('App\v1\MagmaVar','ga_code','ga_code')
+                    ->select('ga_code','cu_status','var_data_date','var_rekom')
+                    ->where('cu_status','Level II (Waspada)')
+                    ->orderBy('var_data_date','desc');
+    }
+
+    public function siaga()
+    {
+        return $this->hasOne('App\v1\MagmaVar','ga_code','ga_code')
+                    ->select('ga_code','cu_status','var_data_date','var_rekom')
+                    ->where('cu_status','Level III (Siaga)')
+                    ->orderBy('var_data_date','desc');
+    }
+
+    public function awas()
+    {
+        return $this->hasOne('App\v1\MagmaVar','ga_code','ga_code')
+                    ->select('ga_code','cu_status','var_data_date','var_rekom')
+                    ->where('cu_status','Level IV (Awas)')
+                    ->orderBy('var_data_date','desc');
     }
 }
