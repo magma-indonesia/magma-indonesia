57a237504498a6d95757cb6b19d6c107f2188137
Martanto <martanto@live.com>
2018-07-16 09:06:50 +0700

    Initiate pengajuan controller

---
+++ app/Http/Controllers/PengajuanController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Pengajuan;
+use Illuminate\Http\Request;
+
+class PengajuanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $pengajuans = Pengajuan::with('pelapor','penjawab')->orderBy('created_at','desc')->paginate(30);
+        return view('pengajuan.index',compact('pengajuans'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Pengajuan  $pengajuan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pengajuan $pengajuan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Pengajuan  $pengajuan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pengajuan $pengajuan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Pengajuan  $pengajuan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pengajuan $pengajuan)
+    {
+        return $request;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Pengajuan  $pengajuan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pengajuan $pengajuan)
+    {
+        //
+    }
+}
