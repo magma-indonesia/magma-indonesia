a516610165bb3b6490f96821819a16cc8ca72b48
Martanto <martanto@live.com>
2018-07-23 16:16:56 +0700

    Fix ui allignment

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,5 +1,5 @@
-        <footer class="footer">
-            <span class="pull-right">
-                MAGMA Indonesia © 2015-2017 PVMBG. All rights reserved.
-            </span>
-        </footer>
+<footer class="footer">
+    <span class="pull-right">
+        MAGMA Indonesia © 2015-2017 PVMBG. All rights reserved.
+    </span>
+</footer>
