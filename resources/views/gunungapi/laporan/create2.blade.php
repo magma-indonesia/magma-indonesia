12d5da3d7fd1abdd1d6b28d950e6ac4a7f61b049
Martanto <martanto@live.com>
2018-07-24 17:11:50 +0700

    Initiate upload foto visual

--- resources/views/gunungapi/laporan/create2.blade.php
+++ resources/views/gunungapi/laporan/create2.blade.php
@@ -6,6 +6,7 @@
 
 @section('add-vendor-css')
     <link rel="stylesheet" href="{{ asset('vendor/bootstrap-datepicker-master/dist/css/bootstrap-datepicker3.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('vendor/croppie/croppie.min.css') }}" />
 @endsection
 
 @section('content-header')
@@ -109,11 +110,27 @@
                                                     </div>
                                                 </div>
                                                 <div class="form-group col-lg-12">
-                                                    <label {{$errors->has('visual_kawah') ? 'class=text-danger' : ''}}>Keterangan Visual Lainnya</label>
-                                                    <div class="row">
-                                                        <div class="col-lg-6 col-xs-12">
-                                                            <textarea placeholder="Kosongi jika tidak ada" name="visual_kawah" class="form-control" rows="3">{{ old('visual_kawah')}}</textarea>
-                                                        </div>
+                                                    <label>Foto Visual</label>
+                                                    <div class="checkbox">
+                                                        <label class="checkbox-inline"><input name="isfoto" value="1" type="radio" class="i-checks isfoto" {{ (old('isfoto') == '1' || empty(old('isfoto'))) ? 'checked' : ''}}> Ada </label>
+                                                    </div>
+                                                    <div class="checkbox">
+                                                        <label class="checkbox-inline"><input name="isfoto" value="0" type="radio" class="i-checks isfoto" {{ old('isfoto') == '0' ? 'checked' : ''}}> Tidak Ada </label>
+                                                    </div>
+                                                </div>
+
+                                                {{-- Extended Foto Visual --}}
+                                                <div class="foto-visual" style="{{ old('isfoto') == '0' ? 'Display:none' : ''}}">
+                                                    <div class="form-group col-lg-12">
+                                                        <label>Upload Foto Visual</label>
+                                                        <div class="upload-message">Upload Foto Visual</div>
+                                                        <div class="upload-photo" style="display:none"></div>
+                                                        <label class="form-control btn btn-primary btn-file">
+                                                            <span class="label-file">Browse </span> 
+                                                            <input accept="image/*" class="file" name="file" type="file" style="display: none;">
+                                                            <input type="hidden" id="imagebase64" name="imagebase64">
+                                                            <input type="hidden" id="filetype" name="filetype"> 
+                                                        </label>
                                                     </div>
                                                 </div>
 
@@ -121,7 +138,7 @@
                                                 <div class="visual-asap" style="{{ old('visual_asap') != 'Teramati' ? 'Display:none;' : '' }}">
                                                     <div class="form-group col-lg-12">
                                                         <label {{$errors->has('tasap_max') || $errors->has('tasap_min')? 'class=text-danger' : ''}}>Tinggi Asap Dari Atas Puncak (meter)</label>
-                                                        <div class="row">
+                                                        <div class="form-group row">
                                                             <div class="col-lg-6 col-xs-12">
                                                                 <div class="input-group">
                                                                     <input placeholder="Minimum" name="tasap_min" class="form-control" type="text" value="{{ empty(old('tasap_min')) ? '' : old('tasap_min') }}">
@@ -154,7 +171,7 @@
                                                     </div>
                                                     <div class="form group col-lg-12">
                                                         <label {{$errors->has('intasap') ? 'class=text-danger' : ''}}>Intensitas Asap</label>
-                                                        <div class="row">
+                                                        <div class="form-group row">
                                                             <div class="col-sm-6">
                                                                 <label class="checkbox-inline"><input name="intasap[]" value="Tipis" type="checkbox" class="i-checks intensitas" {{ (is_array(old('intasap')) AND in_array('Tipis',old('intasap'))) ? 'checked' : ''}}> Tipis </label>
                                                                 <label class="checkbox-inline"><input name="intasap[]" value="Sedang" type="checkbox" class="i-checks intensitas" {{ (is_array(old('intasap')) AND in_array('Sedang',old('intasap'))) ? 'checked' : ''}}> Sedang </label>
@@ -164,7 +181,7 @@
                                                     </div>
                                                     <div class="form group col-lg-12">
                                                         <label {{$errors->has('tekasap') ? 'class=text-danger' : ''}}>Tekanan Asap</label>
-                                                        <div class="row">
+                                                        <div class="form-group row">
                                                             <div class="col-sm-6">
                                                                 <label class="checkbox-inline"><input name="tekasap[]" value="Lemah" type="checkbox" class="i-checks intensitas" {{ (is_array(old('tekasap')) AND in_array('Tipis',old('tekasap'))) ? 'checked' : ''}}> Tipis </label>
                                                                 <label class="checkbox-inline"><input name="tekasap[]" value="Sedang" type="checkbox" class="i-checks intensitas" {{ (is_array(old('tekasap')) AND in_array('Sedang',old('tekasap'))) ? 'checked' : ''}}> Sedang </label>
@@ -172,6 +189,15 @@
                                                             </div>
                                                         </div>
                                                     </div>         
+                                                </div>
+
+                                                <div class="form-group col-lg-12">
+                                                    <label {{$errors->has('visual_kawah') ? 'class=text-danger' : ''}}>Keterangan Visual Lainnya</label>
+                                                    <div class="row">
+                                                        <div class="col-xs-12">
+                                                            <textarea placeholder="Kosongi jika tidak ada" name="visual_kawah" class="form-control" rows="3">{{ old('visual_kawah')}}</textarea>
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
                                             <hr>
@@ -195,6 +221,8 @@
 @section('add-vendor-script')
     <script src="{{ asset('vendor/moment/moment.js') }}"></script>
     <script src="{{ asset('vendor/bootstrap-datepicker-master/dist/js/bootstrap-datepicker.min.js') }}"></script>
+    <script src="{{ asset('vendor/croppie/croppie.min.js') }}"></script>
+    <script src="{{ asset('vendor/exif-js/exif.js') }}"></script>
 @endsection
 
 @section('add-script')
@@ -203,6 +231,56 @@
 
             $('.asap').on('ifChecked', function(e) {
                 $(this).val() == 'Teramati' ? $('.visual-asap').show() : $('.visual-asap').hide();
+            });
+
+            $('.isfoto').on('ifChecked', function(e) {
+                $(this).val() == '1' ? $('.foto-visual').show() : $('.foto-visual').hide();
+            });
+
+            $uploadCrop = $('.upload-photo').croppie({
+                enableExif: true,
+                viewport: {
+                    width: 480,
+                    height: 270
+                },
+                boundary: {
+                    width:490,
+                    height:280
+                }
+            });
+
+            $('input.file').on('change', function() {
+                $('.upload-message').hide();
+                $('.upload-photo').show();
+            
+                var input = $(this),
+                    label = input.val().replace(/\\/g, '/').replace(/.*\//, ''),
+                    filetype = '.'+label.split('.').pop();
+                    reader = new FileReader();
+
+                $('.label-file').html(label);
+                console.log('Nama file : '+label+', tipe : '+filetype);
+                $('#filetype').val(filetype);
+
+                reader.onload = function (e) {
+                    $uploadCrop.croppie('bind', {
+                        url: e.target.result,
+                        orientation: 1
+                    }).then(function(){
+                        console.log('Binding Berhasil');
+                    });
+                }
+
+                reader.readAsDataURL(this.files[0]);
+                
+                $uploadCrop.croppie('result', {
+                    type: 'canvas',
+                    size: 'viewport'
+                }).then(function (resp){
+                    $('#imagebase64').val(resp);
+                    console.log($('#imagebase64').val());
+                });
+                
             });
 
         });
