286f71ee1f1e460fcd4afbcfdaad28b797f08578
Martanto <martanto@live.com>
2018-07-27 19:28:14 +0700

    Add ecport route
    - ven
    - absensi
    - vona

--- routes/export.php
+++ routes/export.php
@@ -3,3 +3,5 @@
 Route::get('/','v1\ExportController@index')->name('index');
 Route::get('/vars','Export\ExportMagmaVar@export')->name('vars');
 Route::get('/ven','Export\ExportMagmaVen@export')->name('ven');
+Route::get('/absensi','Export\ExportAbsensi@export')->name('absensi');
+Route::get('/vona','Export\ExportVona@export')->name('vona');
