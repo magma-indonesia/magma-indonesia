d3beb56d9b4236b1e435fc8a4919fe5bb00a0310
Martanto <martanto@live.com>
2018-07-30 21:16:37 +0700

    Add provider for base64image validation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,6 +23,32 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->setLocalToIndonesia();
+
+        Validator::extend('base64image', function ($attribute, $value, $parameters, $validator) {
+            $explode = explode(',', $value);
+            $allow = ['jpg','jpeg'];
+            $format = str_replace(
+                [
+                    'data:image/',
+                    ';',
+                    'base64',
+                ],
+                [
+                    '', '', '',
+                ],
+                $explode[0]
+            );
+
+            if (!in_array($format, $allow)) {
+                return false;
+            }
+
+            if (!preg_match('%^[a-zA-Z0-9/+]*={0,2}$%', $explode[1])) {
+                return false;
+            }
+
+            return true;
+        });
     }
 
     /**
