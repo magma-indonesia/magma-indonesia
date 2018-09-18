7bcdbbf331c739cab26119a600c864af5608c5a5
Martanto <martanto@live.com>
2018-09-18 16:38:04 +0700

    Add locale

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,7 +9,7 @@ class HomeController extends Controller
     public function __construct(Request $request)
     {
 
-        $this->middleware('guest');
+        $this->middleware('set.locale');
 
     }
     /**
