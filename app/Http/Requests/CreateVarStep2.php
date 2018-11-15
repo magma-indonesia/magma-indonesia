dab0491cba2efa04a749173061bae4c941288db5
Martanto <martanto@live.com>
2018-11-15 11:15:10 +0700

    Change validator for tasap_min dan tasap_max

--- app/Http/Requests/CreateVarStep2.php
+++ app/Http/Requests/CreateVarStep2.php
@@ -36,7 +36,8 @@ class CreateVarStep2 extends FormRequest
             'max' => 'Maximum ukuran file 2MB',
             'base64image' => 'Format gambar harus <b>JPG</b> atau <b>JPEG</b>',
             'lte' => 'Tinggi Asap Minimum <b>harus kurang dari</b> Tinggi Asap Maksimum',
-            'digits_between' => 'Tinggi Asap <b>minimal</b> 50 meter dan <b>maksimum</b> 10.000 meter',
+            'tasap_min.between' => 'Tinggi Asap Minimum <b>minimal</b> 50 meter dan <b>maksimum</b> 10.000 meter',
+            'tasap_max.between' => 'Tinggi Asap Maximum <b>minimal</b> 50 meter dan <b>maksimum</b> 10.000 meter',
             'foto.required_if' => 'File Foto <b>wajib disertakan</b> jika pilihan Foto Visual (Ada)',
             'wasap.required_if' => 'Warna Asap <b>wajib diisi</b> jika Visual Asap Kawah <b>Teramati</b>',
             'intasap.required_if' => 'Intensitas Asap <b>wajib diisi</b> jika Visual Asap Kawah <b>Teramati</b>',
@@ -52,8 +53,8 @@ class CreateVarStep2 extends FormRequest
     public function rules(Request $request)
     {
 
-        $tasap_max = $request->visual_asap == 'Teramati' ? '|digits_between:3,5' : '';
-        $tasap_min = $request->visual_asap == 'Teramati' ? '|digits_between:3,5|lte:tasap_max' : '';
+        $tasap_max = $request->visual_asap == 'Teramati' ? '|numeric|between:50,10000' : '';
+        $tasap_min = $request->visual_asap == 'Teramati' ? '|numeric|between:50,10000|lte:tasap_max' : '';
         $foto = $request->hasfoto ? '|image|mimes:jpeg,jpg|max:2048' : '';
 
         return [
