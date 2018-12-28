ebcd649602d557c3e466da351d87e22904b0563d
Martanto <martanto@live.com>
2018-12-28 15:39:08 +0700

    Initiate magma_roq v1

---
+++ app/Http/Controllers/v1/MagmaRoqController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\v1\MagmaRoq;
+
+class MagmaRoqController extends Controller
+{
+    /**
+     * Adding middleware for protecttion
+     * 
+     * @return boolean
+     */
+    public function __construct()
+    {
+        $this->middleware(
+            'role:Super Admin|Staff MGB',
+            ['except' => 'index','show']
+        );
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $roqs = MagmaRoq::orderBy('datetime_utc','desc')
+            ->paginate(30,['*'],'roq_page');
+
+        return view('v1.gempabumi.index',compact('roqs'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $roq = MagmaRoq::findOrFail($id);
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
