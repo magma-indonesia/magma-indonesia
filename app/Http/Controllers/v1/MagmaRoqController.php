1499b87a89e41da5677ef20565af229480a44549
Martanto <martanto@live.com>
2018-12-29 06:42:25 +0700

    Add validation rules

--- app/Http/Controllers/v1/MagmaRoqController.php
+++ app/Http/Controllers/v1/MagmaRoqController.php
@@ -53,6 +53,25 @@ class MagmaRoqController extends Controller
      */
     public function store(Request $request)
     {
+        $this->validate($request,[
+            'datetime_wib' => 'required|date_format:Y-m-d H:i|before:tomorrow',
+            'lat_lima' => 'required|numeric|between:-10,12',
+            'lat_lima' => 'required|numeric|between:94,142',
+            'magnitude' => 'required|numeric|between:0,9',
+            'depth' => 'required|numeric|between:9,500',
+            'area' => 'required',
+            'koter' => 'required',
+            'roq_tanggapan' => 'required|boolean',
+            'roq_title' => 'required_if:roq_tanggapan,1',
+            'roq_intro' => 'required_if:roq_tanggapan,1',
+            'roq_konwil' => 'required_if:roq_tanggapan,1',
+            'roq_mekanisme' => 'required_if:roq_tanggapan,1',
+            'roq_efek' => 'required_if:roq_tanggapan,1',
+            'roq_rekom' => 'required_if:roq_tanggapan,1',
+            'roq_source' => 'required_if:roq_tanggapan,1|array',
+            'roq_source.*' => 'required_if:roq_tanggapan,1|in:BMKG,GFZ,USGS',
+            'roq_tsunami' => 'required_if:roq_tanggapan,1|boolean'
+        ]);
         return $request;
     }
 
@@ -76,7 +95,7 @@ class MagmaRoqController extends Controller
     public function edit($id)
     {
         $roq = MagmaRoq::findOrFail($id);
-
+        return $roq;
     }
 
     /**
