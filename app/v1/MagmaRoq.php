c62db3b069e2f63c56f44aacc9daafac3280ccba
Martanto <martanto@live.com>
2018-12-31 13:00:40 +0700

    Update fillable and guarded attributes

--- app/v1/MagmaRoq.php
+++ app/v1/MagmaRoq.php
@@ -14,4 +14,39 @@ class MagmaRoq extends Model
 
     protected $table = 'magma_roq';
 
+    protected $guarded = [
+        'no'
+    ];
+
+    protected $fillable = [
+        'datetime_wib',
+        'id_lap',
+        'datetime_wib_str',
+        'datetime_utc',
+        'magnitude',
+        'magtype',
+        'depth',
+        'dep_unit',
+        'lon_lima',
+        'lat_lima',
+        'latlon_text',
+        'area',
+        'koter',
+        'mmi',
+        'nearest_volcano',
+        'roq_tanggapan',
+        'roq_title',
+        'roq_tsu',
+        'roq_intro',
+        'roq_konwil',
+        'roq_mekanisme',
+        'roq_efek',
+        'roq_rekom',
+        'roq_source',
+        'roq_nama_pelapor',
+        'roq_nip_pelapor',
+        'roq_nama_pemeriksa',
+        'roq_nip_pemeriksa',
+    ];
+
 }
