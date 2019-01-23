5fd70844befee8a1afde99d510a8356d19150046
Martanto <martanto@live.com>
2019-01-23 16:31:38 +0700

    Add show last data for v1 api

--- app/Http/Controllers/Api/v1/MagmaVarController.php
+++ app/Http/Controllers/Api/v1/MagmaVarController.php
@@ -11,6 +11,11 @@ use App\Http\Resources\v1\MagmaVarCollection;
 
 class MagmaVarController extends Controller
 {
+    public function __construct()
+    {
+        ini_set('max_execution_time', 60);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -18,7 +23,23 @@ class MagmaVarController extends Controller
      */
     public function index(Request $request)
     {
-        $vars = Gadd::with('var')->paginate(5);
+        $vars = Gadd::with('var')->get();
         return new MagmaVarCollection($vars);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Vona  $vona
+     * @return \Illuminate\Http\Response
+     */
+    public function show($code)
+    {
+        $var = MagmaVar::where('ga_code',$code)
+                    ->orderBy('var_data_date','desc')
+                    ->orderBy('periode','desc')
+                    ->first();
+
+        return new MagmaVarResource($var);
     }
 }
