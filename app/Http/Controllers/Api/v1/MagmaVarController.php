bb7d22fc7b0686e80d5abd5723c4ff510ac90b39
Martanto <martanto@live.com>
2019-01-24 10:42:16 +0700

    removing validation for magma var v1

--- app/Http/Controllers/Api/v1/MagmaVarController.php
+++ app/Http/Controllers/Api/v1/MagmaVarController.php
@@ -5,17 +5,13 @@ namespace App\Http\Controllers\Api\v1;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\v1\MagmaVar;
+use App\v1\MagmaVarOptimize;
 use App\v1\Gadd;
 use App\Http\Resources\v1\MagmaVarResource;
 use App\Http\Resources\v1\MagmaVarCollection;
 
 class MagmaVarController extends Controller
 {
-    public function __construct()
-    {
-        ini_set('max_execution_time', 60);
-    }
-
     /**
      * Display a listing of the resource.
      *
@@ -36,14 +32,14 @@ class MagmaVarController extends Controller
     public function show($code, $noticenumber = null)
     {
         if ($noticenumber) {
-            $var = MagmaVar::where('ga_code',$code)
+            $var = MagmaVarOptimize::where('ga_code',$code)
                     ->where('var_noticenumber',$noticenumber)
                     ->firstOrFail();
             
             return new MagmaVarResource($var);
         }
 
-        $var = MagmaVar::where('ga_code',$code)
+        $var = MagmaVarOptimize::where('ga_code',$code)
                     ->orderBy('var_data_date','desc')
                     ->orderBy('periode','desc')
                     ->first();
