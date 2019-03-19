6cf60d726c49e668d1f440af05a8b567373cbfa2
Martanto <martanto@live.com>
2019-03-19 16:23:42 +0700

    Revert "Update zoom for mobile"
    This reverts commit bde67a08b90f4f567931fca2a71a71f22048f426.

--- resources/views/v1/home/index.blade.php
+++ resources/views/v1/home/index.blade.php
@@ -279,9 +279,9 @@
             function zoomResponsive() {
                 var width = screen.width;
                     if (width <= 767) {
-                        return 5;
+                        return 10;
                     }
-                    return 5;
+                    return 3;
             }
             
             // Map Inititiation
@@ -290,7 +290,7 @@
                         center: [0, 116.1475],
                         zoom: zoomResponsive(),
                         attributionControl:false
-                    }).setMinZoom(3)
+                    }).setMinZoom(5)
                     .setMaxZoom(11);
 
             // Add Layers
