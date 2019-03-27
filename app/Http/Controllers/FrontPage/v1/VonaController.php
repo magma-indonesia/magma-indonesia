cf34a70124d5de60522f5a396d1eb3fc0f41c8bb
Martanto <martanto@live.com>
2019-03-27 10:08:31 +0700

    Update v1 VonaController

---
+++ app/Http/Controllers/FrontPage/v1/VonaController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\FrontPage\v1;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Cache;
+use App\v1\Vona;
+
+class VonaController extends Controller
+{
+    //
+    public function index(Request $request)
+    {
+        \Carbon\Carbon::setLocale('en');
+
+        $vona = Vona::select('no','sent','log')
+                    ->where('sent',1)
+                    ->orderBy('log','desc')
+                    ->first();
+
+        $page = $request->has('page') ? $request->page : 1;
+
+        $vonas = Cache::remember('v1/home/vona-'.$vona->no.'-'.$page, 30, function() {
+            return Vona::where('sent',1)
+                ->orderBy('log','desc')
+                ->paginate(15);
+        });
+
+        return view('v1.home.vona', compact('vonas'));
+    }
+
+    public function show($id)
+    {
+        $vona = Vona::where('no',$id)->first();
+        $vona->addView();
+        return view('v1.home.vona-show',compact('vona'));
+    }
+}
