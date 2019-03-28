6b5294c2394dc13529f82d7f0da0ca991e8f95ab
Martanto <martanto@live.com>
2019-03-28 20:06:49 +0700

    Add cache for ven

--- app/Http/Controllers/FrontPage/v1/GunungApiController.php
+++ app/Http/Controllers/FrontPage/v1/GunungApiController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\FrontPage\v1;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Cache;
 use App\v1\MagmaVen;
 use DB;
 
@@ -11,18 +12,28 @@ class GunungApiController extends Controller
 {
     public function indexVen(Request $request)
     {
-        $vens = MagmaVen::with('gunungapi:ga_code,ga_nama_gapi,ga_zonearea,ga_elev_gapi','user:vg_nip,vg_nama')
-                ->orderBy('erupt_tgl','desc')
-                ->paginate(30);
+        $ven = MagmaVen::select('erupt_id')
+                ->orderBy('erupt_id','desc')
+                ->first();
+
+        $page = $request->has('page') ? $request->page : 1;
+
+        $vens = Cache::remember('v1/home/vens-'.$ven->erupt_id.'-page-'.$page, 120, function() {
+            return MagmaVen::with('gunungapi:ga_code,ga_nama_gapi,ga_zonearea,ga_elev_gapi','user:vg_nip,vg_nama')
+                    ->orderBy('erupt_tgl','desc')
+                    ->paginate(30);
+        });
 
         $grouped = $vens->groupBy('erupt_tgl');
 
-        $counts = MagmaVen::with('gunungapi:ga_code,ga_nama_gapi')
-                ->select('ga_code','erupt_tgl', DB::raw('count(*) as total'))
-                ->where('erupt_tgl','like','%'.now()->format('Y').'%')
-                ->groupBy('ga_code')
-                ->orderBy('total','desc')
-                ->get();
+        $counts = Cache::remember('v1/home/vens-count-'.$ven->erupt_id, 120, function () {
+            return MagmaVen::with('gunungapi:ga_code,ga_nama_gapi')
+                    ->select('ga_code','erupt_tgl', DB::raw('count(*) as total'))
+                    ->where('erupt_tgl','like','%'.now()->format('Y').'%')
+                    ->groupBy('ga_code')
+                    ->orderBy('total','desc')
+                    ->get();
+        });
 
         return view('v1.home.letusan',compact('vens','grouped','counts'));
     }
