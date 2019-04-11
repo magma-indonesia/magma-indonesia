52332eb1e3f9963ddc0ebedfd901f357abf39246
Martanto <martanto@live.com>
2019-04-11 09:56:06 +0700

    Update navbar home

--- resources/views/includes/slim-navbar.blade.php
+++ resources/views/includes/slim-navbar.blade.php
@@ -7,33 +7,21 @@
                     <span>Peta </span>
                 </a>
             </li>
-            <li class="nav-item with-sub {{ active('v1.vona.index') }}">
-                <a class="nav-link" href="#">
-                    <i class="icon ion-plane"></i>
-                    <span>VONA </span>
-                </a>
-                <div class="sub-item">
-                    <ul>
-                        <li><a href="{{ route('v1.vona.index') }}">Issued</a></li>
-                        <li><a href="#">Subscription</a></li>
-                    </ul>
-                </div>
-            </li>
-            <li class="nav-item {{ active('v1.press.index') }}">
-                <a class="nav-link" href="{{ route('v1.press.index') }}">
-                    <i class="icon ion-ios-bookmarks-outline"></i>
-                    <span>Press Release </span>
-                </a>
-            </li>
             <li class="nav-item with-sub {{ active(['v1.gunungapi.ven','v1.gunungapi.var']) }}">
                 <a class="nav-link" href="#">
                     <i class="icon icon-volcano-warning"></i>
                     <span>Gunung Api </span>
                 </a>
                 <div class="sub-item">
+                    <label class="label-section">Informasi</label>
                     <ul>
-                        <li><a href="{{ route('v1.gunungapi.ven') }}">Informasi Letusan</a></li>
                         <li><a href="{{ route('v1.gunungapi.var') }}">Laporan Aktivitas</a></li>
+                        <li><a href="{{ route('v1.gunungapi.ven') }}">Letusan (VEN)</a></li>
+                    </ul>
+                    <label class="label-section mg-t-15">VONA <i class="icon ion-plane"></i></label>
+                    <ul>
+                        <li><a href="{{ route('v1.vona.index') }}">Issued</a></li>
+                        <li><a href="#">Subscription</a></li>
                     </ul>
                 </div>
             </li>
@@ -58,6 +46,24 @@
                         <li><a href="#">COMING SOON!</a></li>
                     </ul>
                 </div>
+            </li>
+            <li class="nav-item with-sub {{ active('v1.vona.index') }}">
+                <a class="nav-link" href="#">
+                    <i class="icon ion-plane"></i>
+                    <span>VONA </span>
+                </a>
+                <div class="sub-item">
+                    <ul>
+                        <li><a href="{{ route('v1.vona.index') }}">Issued</a></li>
+                        <li><a href="#">Subscription</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li class="nav-item {{ active('v1.press.index') }}">
+                <a class="nav-link" href="{{ route('v1.press.index') }}">
+                    <i class="icon ion-ios-bookmarks-outline"></i>
+                    <span>Press Release </span>
+                </a>
             </li>
         </ul>
     </div>
