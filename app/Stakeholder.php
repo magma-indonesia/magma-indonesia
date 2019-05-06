022245d65e56e7d82cdb2e8f24d7f5922d01327a
Martanto <martanto@live.com>
2019-05-06 13:58:40 +0700

    Create Stakeholder.php

---
+++ app/Stakeholder.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class Stakeholder extends Authenticatable implements JWTSubject
+{
+    use Notifiable;
+
+    protected $casts = [
+        'status' => 'boolean'
+    ];
+
+    protected $dates = [
+        'expired_at',
+    ];    
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'app_name', 
+        'uuid', 
+        'organisasi',
+        'api_type',
+        'api_token',
+        'status',
+        'kontak_nama',
+        'kontak_phone',
+        'kontak_nama',
+        'status',
+        'expired_at'
+    ];
+
+    protected $guarded  = [
+        'id'
+    ];
+
+    protected $hidden  = [
+        'api_token',
+        'api_type'
+    ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [
+            'source' => 'MAGMA Indonesia',
+            'api_version' => 'v1'
+        ];
+    }
+}
