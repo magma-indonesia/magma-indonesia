c900ad6606a00cea82313b23f4cb335f481b8716
Martanto <martanto@live.com>
2019-05-25 10:44:06 +0700

    Update index.blade.php

--- resources/views/gunungapi/laporan/index.blade.php
+++ resources/views/gunungapi/laporan/index.blade.php
@@ -39,13 +39,13 @@
                                 </thead>
                                 <tbody>
                                     @foreach ($gadds as $gadd)
-                                    <tr title="{{ $gadd->latestVar->status_deskripsi }}">
-                                        <td class="{{ $gadd->latestVar->status }}">{{ $gadd->name }}</td>
+                                    <tr title="{{ $gadd->latest_vars->status_deskripsi }}">
+                                        <td class="{{ $gadd->latest_vars->status }}">{{ $gadd->name }}</td>
                                         <td>
-                                            <span class="pie">{{ $gadd->latestVar->var_data_date->formatLocalized('%A, %d %B %Y').', '.$gadd->latestVar->periode }}</span>
+                                            <span class="pie">{{ $gadd->latest_vars->var_data_date->formatLocalized('%A, %d %B %Y').', '.$gadd->latest_vars->periode }}</span>
                                         </td>  
                                         <td>
-                                            <a href="{{ route('chambers.laporan.show',$gadd->latestVar->noticenumber ) }}" target="_blank" class="btn btn-sm btn-magma btn-outline" style="margin-right: 3px;">View</a>
+                                            <a href="{{ route('chambers.laporan.show',$gadd->latest_vars->noticenumber ) }}" target="_blank" class="btn btn-sm btn-magma btn-outline" style="margin-right: 3px;">View</a>
                                         </td>
                                     </tr>
                                     @endforeach
