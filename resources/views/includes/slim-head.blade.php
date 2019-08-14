97170a13707f13ae425ed1f190ccdfc68097d7ba
Martanto <martanto@live.com>
2019-08-15 06:49:41 +0700

    Update slim-head.blade.php

--- resources/views/includes/slim-head.blade.php
+++ resources/views/includes/slim-head.blade.php
@@ -24,7 +24,7 @@
     <!-- Facebook -->
     <meta property="og:type" content="website"/>
     <meta property="og:url" content="{{ config('app.url') }}">
-    <meta property="og:title" content="{{ config('app.name') }}">
+    <meta property="og:title" content="{{ config('app.name') }} | {{ config('app.tag_line') }}">
     <meta property="og:description" content="@yield('title')">
     <meta property="og:image" content="{{ asset('snapshot.png') }}">
     <meta property="og:image:secure_url" content="{{ asset('snapshot.png') }}">
