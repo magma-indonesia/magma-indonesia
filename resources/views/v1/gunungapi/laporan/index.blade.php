b522c613a3ab1aae5145e6972109afe3358dc243
Martanto <martanto@live.com>
2019-09-30 14:21:47 +0700

    Update index.blade.php

--- resources/views/v1/gunungapi/laporan/index.blade.php
+++ resources/views/v1/gunungapi/laporan/index.blade.php
@@ -47,8 +47,11 @@
                 </div>
                 <div class="panel-body float-e-margins">
                     <div class="row">
-                        <div class="col-md-4 col-lg-4 col-sm-12 col-xs-12">
+                        <div class="col-md-3 col-lg-3 col-sm-12 col-xs-12">
                             <a href="{{ route('chambers.v1.gunungapi.laporan.filter') }}"" class="btn btn-outline btn-block btn-magma" type="button">Filter Laporan</a>
+                        </div>
+                        <div class="col-md-3 col-lg-3 col-sm-12 col-xs-12">
+                            <a href="{{ route('chambers.v1.gunungapi.laporan.filter.gempa') }}"" class="btn btn-outline btn-block btn-magma" type="button">Filter Berdasarkan Gempa</a>
                         </div>
                     </div>
                 </div>
