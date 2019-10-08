4ff7df5a7f8052410c21540f185517ce92d75485
Martanto <martanto@live.com>
2019-10-08 19:35:59 +0700

    Update session.php

--- config/session.php
+++ config/session.php
@@ -124,7 +124,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        str_slug(env('APP_NAME', 'laravel'), '_').'_session'
+        '__Secure-'+str_slug(env('APP_NAME', 'laravel'), '_')
     ),
 
     /*
@@ -151,7 +151,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', null),
+    'domain' => env('SESSION_DOMAIN', 'magma.esdm.go.id'),
 
     /*
     |--------------------------------------------------------------------------
