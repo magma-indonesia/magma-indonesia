44657a0e44dd174e00e62bb50e52a538ef1947ea
Martanto <martanto@live.com>
2019-11-03 07:17:18 +0700

    Update ChamberController.php

--- app/Http/Controllers/ChamberController.php
+++ app/Http/Controllers/ChamberController.php
@@ -6,12 +6,6 @@ use Illuminate\Http\Request;
 
 class ChamberController extends Controller
 {
-    //
-    public function __construct(Request $request)
-    {
-        $this->middleware('auth');
-    }
-
     public function index()
     {
 
