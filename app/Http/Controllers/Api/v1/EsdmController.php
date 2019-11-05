6cc8b237618bd3b59161250bebc614c6b34b2201
Martanto <martanto@live.com>
2019-11-05 15:43:28 +0700

    Create EsdmController.php

---
+++ app/Http/Controllers/Api/v1/EsdmController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+USE App\Http\Controllers\Api\v1\HomeController;
+
+class EsdmController extends HomeController
+{
+    public function index()
+    {
+        return [
+            'gunung_api' => $this->gunungapiStatus(),
+            'gerakan_tanah' => $this->gerakanTanahLatest(),
+            'gempa_bumi' => $this->gempaBumiLatest()
+        ];
+    }
+}
