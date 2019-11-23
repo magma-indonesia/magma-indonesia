87221cd6cb3aab6df6aa71c886272082ae0e3800
Martanto <martanto@live.com>
2019-11-23 15:59:12 +0700

    Update ImportFotoVisual.php

--- app/Http/Controllers/Import/ImportFotoVisual.php
+++ app/Http/Controllers/Import/ImportFotoVisual.php
@@ -61,6 +61,8 @@ class ImportFotoVisual extends Import
                     ],[
                         'filename_3' => $filename
                     ]);
+
+                    $image->destroy();
             
                     return $this;
                 }
