a302ba2eab1f7334b3772fb272fe9761bd39798c
Martanto <martanto@live.com>
2019-11-23 16:14:04 +0700

    Update ImportFotoVisual.php

--- app/Http/Controllers/Import/ImportFotoVisual.php
+++ app/Http/Controllers/Import/ImportFotoVisual.php
@@ -6,6 +6,7 @@ use App\VarVisual;
 use Illuminate\Http\Request;
 use Exception;
 use Image;
+use Ping;
 use Illuminate\Support\Facades\Storage;
 use App\Traits\ImportHelper;
 
@@ -47,35 +48,46 @@ class ImportFotoVisual extends Import
 
     protected function downloadFotoVisual($visual)
     {
-        try {
-            if ($visual->file_old) {
-                $image = Image::make($visual->file_old);     
+        if ($visual->file_old) {
+
+            $health = Ping::check($visual->file_old);
+
+            if ($health == 200) {
+
+                try {
+                    $image = Image::make($visual->file_old);    
+                } 
+    
+                catch (Exception $e) {
+                    $visual->file_old = null;
+                    $visual->save();
+                    return $this;
+                }
+    
                 $filename = sha1(uniqid()).'.png';
     
                 if (Storage::disk('var_visual')->put($visual->var->code_id.'/'.$filename, $image->stream()))
                 {
                     Storage::disk('var_visual')->put($visual->var->code_id.'/thumbs/'.$filename, $image->widen(150)->stream());
-
+    
                     VarVisual::update([
                         'noticenumber_id' => $visual->noticenumber,
                     ],[
                         'filename_3' => $filename
                     ]);
-
+    
                     $image->destroy();
             
                     return $this;
                 }
             }
 
-            return $this;
-        }
-
-        catch (Exception $e) {
             $visual->file_old = null;
             $visual->save();
             return $this;
         }
+
+        return $this;
     }
 
 }
