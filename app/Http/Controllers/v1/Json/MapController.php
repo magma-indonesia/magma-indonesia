bde6d2371d8d8fd44e8ac36f65590d39e1c02eec
Martanto <martanto@live.com>
2020-01-31 14:29:51 +0700

    Update MapController.php

--- app/Http/Controllers/v1/Json/MapController.php
+++ app/Http/Controllers/v1/Json/MapController.php
@@ -96,7 +96,7 @@ class MapController extends Controller
             'success' => '1',
             'data' => [
                 'laporan' => [
-                    'peta' => empty($gertan->tanggapan->qls_geo) ? 'https://magma.vsi.esdm.go.id/img/empty_sgt.png' : str_replace('http','https',$gertan->tanggapan->qls_geo),
+                    'peta' => $this->getFotoHeaderGertan($gertan),
                     'pelapor' => $gertan->crs_usr,
                     'judul' => 'Laporan Tanggapan Gerakan Tanah di '.$gertan->crs_vil.', '.$gertan->crs_rgn.', '.$gertan->crs_cty.', '.$gertan->crs_prv,
                     'updated_at' => 'Diperbarui pada tanggal '.$gertan->crs_log->formatLocalized('%d %B %Y').' pukul '.$gertan->crs_log->format('H:i:s').' WIB',
@@ -248,6 +248,17 @@ class MapController extends Controller
         return empty($var) ?
             response()->json(['success' => '0', 'message' => 'Data VAR tidak ditemukan'], 500) : 
             $this;
+    }
+
+    protected function getFotoHeaderGertan($gertan)
+    {
+        if ($gertan->tanggapan->qls_pst) 
+            return str_replace('http','https',$gertan->tanggapan->qls_pst);
+
+        if ($gertan->tanggapan->qls_geo) 
+            return str_replace('http','https',$gertan->tanggapan->qls_geo);
+
+        return 'https://magma.vsi.esdm.go.id/img/empty_sgt.png';
     }
 
 }
