8b055160c4262523795ab647f655ae3b5743f6d6
Martanto <martanto@live.com>
2020-07-21 19:36:33 +0700

    Update MagmaVarController.php

--- app/Http/Controllers/Api/v1/MagmaVarController.php
+++ app/Http/Controllers/Api/v1/MagmaVarController.php
@@ -10,6 +10,7 @@ use App\v1\Gadd;
 use App\Http\Resources\v1\MagmaVarResource;
 use App\Http\Resources\v1\MagmaVarCollection;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
 
 class MagmaVarController extends Controller
 {
@@ -20,11 +21,26 @@ class MagmaVarController extends Controller
      */
     public function index(Request $request)
     {
-        $last = OldVar::select('no')->orderBy('no','desc')->first();
-        $page = $request->has('page') ? $request->page : 1;
+        $last_var = OldVar::select('no','var_log')->orderBy('no','desc')->first();
 
-        $vars = Cache::remember('v1/api/vars-'.$last->no.'-page-'.$page, 60, function() {
-            return Gadd::with('var')->paginate(5);
+        $gadds = Cache::remember('v1/home/gadd', 120, function() {
+            return Gadd::select(
+                'ga_code','ga_nama_gapi','ga_kab_gapi',
+                'ga_prov_gapi','ga_koter_gapi','ga_elev_gapi',
+                'ga_lon_gapi','ga_lat_gapi','ga_status')
+            ->whereNotIn('ga_code',['TEO'])
+            ->orderBy('ga_nama_gapi','asc')
+            ->get();
+        });
+
+        $ga_code = $gadds->pluck('ga_code');
+
+        $vars = Cache::remember('v1/home/var:'.strtotime($last_var->var_log), 60, function() use($ga_code) {
+            return OldVar::select(DB::raw('t.*'))
+                ->from(DB::raw('(SELECT ga_code,cu_status,var_data_date,periode,var_perwkt,var_noticenumber,var_nama_pelapor FROM magma_var ORDER BY var_data_date DESC, periode DESC ) t'))
+                ->whereIn('ga_code',$ga_code)
+                ->groupBy('t.ga_code')
+                ->get();
         });
         
         return new MagmaVarCollection($vars);
