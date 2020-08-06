8dc0b93be5802b570efa7a6c8024f0ea548188a7
Martanto <martanto@live.com>
2020-08-06 14:59:17 +0700

    Update GlossaryController.php

--- app/Http/Controllers/GlossaryController.php
+++ app/Http/Controllers/GlossaryController.php
@@ -57,7 +57,7 @@ class GlossaryController extends Controller
     {
         $this->validate($request, [
             'judul' => 'required|unique:glossaries|max:200',
-            'deskripsi' => 'required|min:100',
+            'deskripsi' => 'required|min:50',
             'referensi' => 'nullable',
             'is_published' => 'required|boolean',
             'files' => 'sometimes|required|array',
