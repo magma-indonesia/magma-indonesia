abdb844f581d4cc764e7a9016ad51d05343a1adf
Martanto <martanto@live.com>
2020-12-01 12:18:08 +0700

    Update ChamberController.php

--- app/Http/Controllers/ChamberController.php
+++ app/Http/Controllers/ChamberController.php
@@ -6,6 +6,7 @@ use App\MagmaVar;
 use App\EqLts;
 use App\StatistikHome;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class ChamberController extends Controller
 {
@@ -15,17 +16,23 @@ class ChamberController extends Controller
 
     public function index()
     {
-        $statistics = StatistikHome::limit(60)->orderBy('date')->get();
-        $statistics_chart = $this->setCategories($statistics)->setSeries()->getCharts();
-        $statistics_sum = StatistikHome::sum('hit');
+        
+        $statistics_chart = Cache::remember('visitor-v2', 10, function () {
+            $statistics = StatistikHome::limit(60)->orderBy('date')->get();
+            return $this->setCategories($statistics)->setSeries()->getCharts();
+        });
+        
+        $statistics_sum = Cache::remember('visitor-sum', 10, function () {
+            return StatistikHome::sum('hit');
+        });
+        
         $vars_count = MagmaVar::count();
         $latest = Magmavar::latest()->first();
         $lts_sum = EqLts::sum('jumlah');
         $latest_lts = EqLts::latest()->first();
         return view('chambers.index', compact(
             'vars_count','latest','lts_sum',
-            'latest_lts', 'statistics', 
-            'statistics_chart', 'statistics_sum'));
+            'latest_lts', 'statistics_chart', 'statistics_sum'));
     }
 
     protected function setCategories($categories)
