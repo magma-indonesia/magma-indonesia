e92cb088bcd3942f540dc1c0f89857c9f8e71ed3
Martanto <martanto@live.com>
2021-02-13 07:26:36 +0700

    Update TrustProxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies = '*';
+    protected $proxies = [
+        '172.16.24.1',
+    ];
 
     /**
      * The current proxy header mappings.
