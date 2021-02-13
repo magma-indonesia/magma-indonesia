a593008b41a6d6c6df5180426972443bd4f1dca3
Martanto <martanto@live.com>
2021-02-13 09:06:08 +0700

    Update TrustProxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,23 +12,20 @@ class TrustProxies extends Middleware
      *
      * @var array
      */
-    protected $proxies = [
-        '172.16.24.1',
-        '172.24.24.3',
-    ];
+    protected $proxies = '**';
 
     /**
      * The current proxy header mappings.
      *
      * @var array
      */
-    // protected $headers = [
-    //     Request::HEADER_FORWARDED => 'FORWARDED',
-    //     Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
-    //     Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
-    //     Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
-    //     Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
-    //     Request::HEADER_X_FORWARDED_ALL
-    // ];
-    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+    protected $headers = [
+        Request::HEADER_FORWARDED => 'FORWARDED',
+        Request::HEADER_X_FORWARDED_FOR => 'X_FORWARDED_FOR',
+        Request::HEADER_X_FORWARDED_HOST => 'X_FORWARDED_HOST',
+        Request::HEADER_X_FORWARDED_PORT => 'X_FORWARDED_PORT',
+        Request::HEADER_X_FORWARDED_PROTO => 'X_FORWARDED_PROTO',
+        Request::HEADER_X_FORWARDED_ALL
+    ];
+    // protected $headers = Request::HEADER_X_FORWARDED_ALL;
 }
