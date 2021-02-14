f943133b8ca00c9ea4eafa9f21088cde703da898
Martanto <martanto@live.com>
2021-02-14 07:07:51 +0700

    Update TestController.php

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,6 +8,6 @@ class TestController extends Controller
 {
     public function index(Request $request)
     {
-        return dd($_SERVER, $request->header(), $request->headers->get('X-Real-IP'), );
+        return dd($_SERVER, $request->header(), $request->ip() );
     }
 }
