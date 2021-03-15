0151b7f25219de8d79fa288e878256843c16ee35
Martanto <martanto@live.com>
2021-03-16 01:36:45 +0700

    Update Blacklist.php

--- app/Http/Middleware/Blacklist.php
+++ app/Http/Middleware/Blacklist.php
@@ -26,7 +26,7 @@ class Blacklist
         ];
 
         $ip = request()->header('X-Forwarded-For') ?: $request->ip();
-        $ips = implode(', ', request()->getClientIps());
+        $ips = implode('|', request()->getClientIps());
        
         if (in_array($ip, $blacklisted)) {
             UpdateBlacklistLog::dispatch($ip);
