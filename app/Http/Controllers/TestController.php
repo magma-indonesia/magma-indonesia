f691ea028beadf5c5533f739ea8b11351277bf16
Martanto <martanto@live.com>
2021-06-29 15:51:23 +0700

    update location

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,11 +8,14 @@ use App\v1\Vona;
 use App\VonaSubscriber;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Stevebauman\Location\Facades\Location;
 
 class TestController extends Controller
 {
     public function index(Request $request)
     {
+        // return $request->ip();
+        return dd(Location::get('103.87.160.58'));
 
         $vona = Vona::first();
 
