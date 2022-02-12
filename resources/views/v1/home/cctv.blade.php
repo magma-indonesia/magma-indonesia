78dc33e91350ff064168f5618b1ffec65057d043
Martanto <martanto@live.com>
2022-02-12 21:38:20 +0700

    Update cctv.blade.php

--- resources/views/v1/home/cctv.blade.php
+++ resources/views/v1/home/cctv.blade.php
@@ -32,6 +32,18 @@ Kamera Gunung Api
 <div class="row row-sm">
     <div class="col-12">
         <div class="card pd-30 mg-b-20">
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/aGA5fbir_4Q" title="PVMBG - CCTV"
+                    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen></iframe>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="row row-sm">
+    <div class="col-12">
+        <div class="card pd-30 mg-b-20">
             <label class="slim-card-title">Daftar CCTV</label>
             <div class="row row-xs">
                 <div class="col-xs-12">
