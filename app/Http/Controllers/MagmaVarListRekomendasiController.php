124544621e5d3103debc0695cebdd01ec18ca2b0
Martanto <martanto@live.com>
2022-02-25 22:54:43 +0700

    wip controller rekomendasi

---
+++ app/Http/Controllers/MagmaVarListRekomendasiController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\v1\MagmaVarListRekomendasi;
+use Illuminate\Http\Request;
+
+class MagmaVarListRekomendasiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\v1\MagmaVarListRekomendasi  $magmaVarListRekomendasi
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MagmaVarListRekomendasi $magmaVarListRekomendasi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\v1\MagmaVarListRekomendasi  $magmaVarListRekomendasi
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MagmaVarListRekomendasi $magmaVarListRekomendasi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\v1\MagmaVarListRekomendasi  $magmaVarListRekomendasi
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MagmaVarListRekomendasi $magmaVarListRekomendasi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\v1\MagmaVarListRekomendasi  $magmaVarListRekomendasi
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MagmaVarListRekomendasi $magmaVarListRekomendasi)
+    {
+        //
+    }
+}
