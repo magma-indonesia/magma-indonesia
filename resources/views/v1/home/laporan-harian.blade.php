4ec069c6f93a4c6625fc57047d0a73faadaf2516
Martanto <martanto@live.com>
2022-03-01 09:17:19 +0700

    Update laporan-harian.blade.php

--- resources/views/v1/home/laporan-harian.blade.php
+++ resources/views/v1/home/laporan-harian.blade.php
@@ -14,51 +14,97 @@ Laporan Harian
 @endsection
 
 @section('main')
-<div class="card card-table">
-    <div class="table-responsive">
-        <table class="table mg-b-0 tx-13">
-            <thead>
-                <tr class="tx-10">
-                    <th class="pd-y-5">No</th>
-                    <th class="wd-10p pd-y-5">Gunung Api</th>
-                    <th class="pd-y-5">Visual</th>
-                    <th class="pd-y-5">Kegempaan</th>
-                    <th class="pd-y-5">Rekomendasi</th>
-                </tr>
-            </thead>
+<div class="nav-statistics-wrapper">
+    <nav class="nav">
+        @foreach ($groupedByStatus as $status => $gadds)
+        @if ($loop->first)
+            <a
+                href="#tab-{{ $loop->index }}"
+                id="home-tab-{{ $loop->index }}"
+                class="nav-link active"
+                data-toggle="tab"
+                role="tab"
+                aria-controls="home-{{ $loop->index }}"
+                aria-selected="true"
+            >{{ $status }}</a>
+        @else
+            <a
+                href="#tab-{{ $loop->index }}"
+                id="home-tab-{{ $loop->index }}"
+                class="nav-link"
+                data-toggle="tab"
+                role="tab"
+                aria-controls="home-{{ $loop->index }}"
+                aria-selected="true"
+            >{{ $status }}</a>
+        @endif
+        @endforeach
+    </nav>
+</div>
 
-            <tbody>
-                @foreach ($gadds as $key => $gadd)
-                <tr>
-                    <td>{{ $key+1 }}</td>
-                    <td>{{ $gadd['name'] }}</td>
-                    <td>
-                        <p>{!! $gadd['visual']['gunung_api'] !!}</p>
-                        @if (!empty($gadd['visual']['letusan']))
-                        <p>{{ $gadd['visual']['letusan'] }}</p>
-                        @endif
-                        @if ($gadd['visual']['guguran']->isNotEmpty())
+<div class="tab-content" id="myTabContent">
+    @foreach ($groupedByStatus as $status => $gadds)
+    <div id="tab-{{ $loop->index }}" role="tabpanel" aria-labelledby="home-tab-{{ $loop->index }}" class="card card-table mg-b-20 tab-pane {{ $loop->first ? 'show active' : '' }}">
+        <div class="card-header">
+            <h6 class="slim-card-title">{{ $status }}</h6>
+        </div>
+        <div class="table-responsive">
+            <table class="table mg-b-0 tx-13">
+                <thead>
+                    <tr class="tx-10">
+                        <th class="pd-y-5">No</th>
+                        <th class="wd-10p pd-y-5">Gunung Api</th>
+                        <th class="pd-y-5">Visual</th>
+                        <th class="pd-y-5">Kegempaan</th>
+                        <th class="pd-y-5">Rekomendasi</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach ($gadds as $key => $gadd)
+                    <tr>
+                        <td>{{ $key+1 }}</td>
+                        <td>{{ $gadd['name'] }}</td>
+                        <td>
+                            <p>{!! $gadd['visual']['gunung_api'] !!}</p>
+                            @if (!empty($gadd['visual']['letusan']))
+                            <p>{{ $gadd['visual']['letusan'] }}</p>
+                            @endif
+                            @if ($gadd['visual']['guguran']->isNotEmpty())
                             @foreach ($gadd['visual']['guguran'] as $guguran)
                             <p>{{ $guguran }}</p>
                             @endforeach
-                        @endif
-                    </td>
-                    <td>Kegempaan</td>
-                    <td>
-                        @if ($gadd['rekomendasi']->count() > 1)
-                        <ol>
-                            @foreach ($gadd['rekomendasi'] as $rekomendasi)
+                            @endif
+                        </td>
+                        <td>
+                            @if ($gadd['kegempaan']->isNotEmpty())
+                            <ul>
+                                @foreach ($gadd['kegempaan'] as $gempa)
+                                <li>{{ $gempa }}</li>
+                                @endforeach
+                            </ul>
+                            @else
+                            Nihil
+                            @endif
+                        </td>
+                        <td>
+                            @if ($gadd['rekomendasi']->count() > 1)
+                            <ol>
+                                @foreach ($gadd['rekomendasi'] as $rekomendasi)
                                 <li>{{ $rekomendasi }}</li>
-                            @endforeach
-                        </ol>
-                        @else
-                        {{ $gadd['rekomendasi']->first() }}
-                        @endif
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+                                @endforeach
+                            </ol>
+                            @else
+                            {{ $gadd['rekomendasi']->first() }}
+                            @endif
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
+    @endforeach
 </div>
+
 @endsection
