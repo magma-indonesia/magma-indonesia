5f7e2ae1803f251ebfc2661684e94ff33ca19930
Martanto <martanto@live.com>
2022-03-02 12:29:12 +0700

    Update StatistikLaporanHarian.php

--- app/StatistikLaporanHarian.php
+++ app/StatistikLaporanHarian.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class StatistikLaporanHarian extends Model
 {
-    //
+    protected $fillable = [
+        'date',
+        'hit',
+    ];
+
+    protected $dates = [
+        'date:Y-m-d',
+    ];
 }
