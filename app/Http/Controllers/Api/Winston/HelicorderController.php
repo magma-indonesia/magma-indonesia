95e2c4e10f70f9adbb5da4e52bce472bd537ee65
Martanto <martanto@live.com>
2022-03-10 18:28:22 +0700

    Update HelicorderController.php

--- app/Http/Controllers/Api/Winston/HelicorderController.php
+++ app/Http/Controllers/Api/Winston/HelicorderController.php
@@ -6,21 +6,46 @@ use App\Gadd;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Seismometer;
+use Illuminate\Support\Facades\Cache;
 use Intervention\Image\Facades\Image;
+use Karlmonson\Ping\Facades\Ping;
 
 class HelicorderController extends Controller
 {
     public function index()
     {
-        return Gadd::has('seismometers')
-            ->with('seismometers')
-            ->select('name','code','tzone')
-            ->get();
+        $health = Ping::check(config('app.winston_url'));
+
+        if ($health !== 200) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Server Seismik sedang offline'
+            ], 500);
+        }
+
+        return Cache::remember('gadd_has_seismometer', 60, function () {
+            return Gadd::has('seismometers')
+                ->with('seismometers')
+                ->select('name', 'code', 'tzone')
+                ->orderBy('name')
+                ->get();
+        });
     }
 
     public function show($scnl, Request $request)
     {
-        if (!Seismometer::where('scnl',$scnl)->first()) {
+        $health = Ping::check(config('app.winston_url'));
+
+        if ($health !== 200) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Server Seismik sedang offline'
+            ], 500);
+        }
+
+        $seismometer = Seismometer::where('scnl', $scnl)->first();
+
+        if (!$seismometer) {
             return [
                 'status' => false,
                 'message' => 'Stasiun tidak ditemukan'
@@ -37,9 +62,12 @@ class HelicorderController extends Controller
         config('app.winston_url') . ':' . config('app.winston_port') . '/heli?code=' . $scnl . '&w='.$width.'&h='. $height.'&lb='. $label.'&tz='.$timezone.'&t1='.$time;
 
         try {
+            $seismometer->increment('hit');
             return Image::make($url)->response();
         } catch (\Throwable $th) {
-            abort(404,'Tidak ditemukan.');
+            return response()->json([
+                'message' => 'Server Seismik sedang mati'
+            ], 500);
         }
     }
 }
