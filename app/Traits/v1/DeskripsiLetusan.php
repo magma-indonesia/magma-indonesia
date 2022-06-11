922a9d2ef3f270e2f1732902bc4f03f44c922c49
Martanto <martanto@live.com>
2022-06-11 11:51:51 +0700

    Update DeskripsiLetusan.php

--- app/Traits/v1/DeskripsiLetusan.php
+++ app/Traits/v1/DeskripsiLetusan.php
@@ -8,41 +8,89 @@ use Illuminate\Support\Facades\URL;
 
 trait DeskripsiLetusan
 {
+    /**
+     * Arah abu letusan
+     *
+     * @param MagmaVen $ven
+     * @return string
+     */
     public function arahAbu(MagmaVen $ven): string
     {
         return str_replace_last(', ', ' dan ', strtolower(implode(', ', $ven->erupt_arh)));
     }
 
+    /**
+     * Intensitas abu
+     *
+     * @param MagmaVen $ven
+     * @return string
+     */
     public function intensitasAbu(MagmaVen $ven): string
     {
         return str_replace_last(', ', ' hingga ', strtolower(implode(', ', $ven->erupt_int)));
     }
 
+    /**
+     * Warna abu letusan
+     *
+     * @param MagmaVen $ven
+     * @return string
+     */
     public function warnaAbu(MagmaVen $ven): string
     {
         return str_replace_last(', ', ' hingga ', strtolower(implode(', ', $ven->erupt_wrn)));
     }
 
-    public function kolomAbu(MagmaVen $ven)
+    /**
+     * Kolom aku letusan
+     *
+     * @param MagmaVen $ven
+     * @return string
+     */
+    public function kolomAbu(MagmaVen $ven): string
     {
-        return "Kolom abu teramati berwarna {$this->warnaAbu($ven)} dengan intensitas {$this->intensitasAbu($ven)} ke arah {$this->arahAbu($ven)}. ";
+        return "Kolom abu teramati berwarna {$this->warnaAbu($ven)} dengan intensitas {$this->intensitasAbu($ven)} ke arah {$this->arahAbu($ven)}.";
     }
 
+    /**
+     * Ketinggian abu letusan
+     *
+     * @param MagmaVen $ven
+     * @return string
+     */
     public function ketinggianMdpl(MagmaVen $ven): string
     {
         return (string) $ven->erupt_tka + $ven->gunungapi->ga_elev_gapi;
     }
 
+    /**
+     * Tinggi kolom abu letusan
+     *
+     * @param MagmaVen $ven
+     * @return string
+     */
     public function ketinggian(MagmaVen $ven): string
     {
         return "Tinggi kolom abu teramati ± {$ven->erupt_tka} m di atas puncak (± {$this->ketinggianMdpl($ven)} m di atas permukaan laut). {$this->kolomAbu($ven)}";
     }
 
+    /**
+     * Visual letusan
+     *
+     * @param MagmaVen $ven
+     * @return string
+     */
     public function visual(MagmaVen $ven): string
     {
         return $ven->erupt_vis ? $this->ketinggian($ven) : 'Visual letusan tidak teramati.';
     }
 
+    /**
+     * Deskripsi gempa
+     *
+     * @param MagmaVen $ven
+     * @return string
+     */
     public function gempa(MagmaVen $ven): string
     {
         if ($ven->erupsi_berlangsung) {
@@ -52,6 +100,12 @@ trait DeskripsiLetusan
         return "Erupsi terekam di seismograf dengan amplitudo maksimum {$ven->erupt_amp} mm dan durasi {$ven->erupt_drs} detik.";
     }
 
+    /**
+     * Tanggal kejadian letusan
+     *
+     * @param MagmaVen $ven
+     * @return string
+     */
     public function tanggal(MagmaVen $ven): string
     {
         $tanggal = Carbon::createFromFormat('Y-m-d', $ven->erupt_tgl)->formatLocalized('%A, %d %B %Y');
@@ -59,31 +113,67 @@ trait DeskripsiLetusan
         return "pada hari $tanggal, pukul $waktu.";
     }
 
+    /**
+     * URL VEN
+     *
+     * @param MagmaVen $ven
+     * @return string
+     */
     public function url(MagmaVen $ven): string
     {
         return URL::signedRoute('v1.gunungapi.ven.show', $ven);
     }
 
+    /**
+     * Deskripsi letusan
+     *
+     * @param MagmaVen $ven
+     * @return string
+     */
     public function deskripsi(MagmaVen $ven): string
     {
         return "Terjadi erupsi G. {$ven->gunungapi->ga_nama_gapi} {$this->tanggal($ven)} {$this->visual($ven)} {$this->gempa($ven)}";
     }
 
+    /**
+     * Deskripsi letusan Telegram
+     *
+     * @param MagmaVen $ven
+     * @return string
+     */
     public function deskripsiTelegram(MagmaVen $ven): string
     {
         return "Terjadi erupsi G. {$ven->gunungapi->ga_nama_gapi} {$this->tanggal($ven)} {$this->visual($ven)} {$this->gempa($ven)} [Detail Laporan]({$this->url($ven)})";
     }
 
+    /**
+     * Tinggi kolom abu letusan buat twitter
+     *
+     * @param MagmaVen $ven
+     * @return string
+     */
     public function ketinggianTwitter(MagmaVen $ven): string
     {
         return "Tinggi kolom abu teramati ± {$ven->erupt_tka} m di atas puncak.";
     }
 
+    /**
+     * Deskripsi visual letusan untuk Twitter
+     *
+     * @param MagmaVen $ven
+     * @return string
+     */
     public function visualTwitter(MagmaVen $ven): string
     {
         return $ven->erupt_vis ? $this->ketinggianTwitter($ven) : 'Visual letusan tidak teramati.';
     }
 
+    /**
+     * Deskripsi letusan untuk Twitter
+     *
+     * @param MagmaVen $ven
+     * @return string
+     */
     public function deskripsiTwitter(MagmaVen $ven): string
     {
         return "Terjadi #erupsi G. {$ven->gunungapi->ga_nama_gapi} {$this->tanggal($ven)} {$this->visualTwitter($ven)} {$this->gempa($ven)} #PVMBG @id_magma";
