2dfb3a4cd815d4bcf4edc94bf7e26d2e552a2b90
Martanto <martanto@live.com>
2022-10-04 16:21:14 +0700

    Update VonaController.php

--- app/Http/Controllers/VonaController.php
+++ app/Http/Controllers/VonaController.php
@@ -3,50 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Vona;
+use App\v1\Vona as VonaOld;
 use App\VonaSubscriber as Subscription;
 use App\Gadd;
+use App\Http\Requests\VonaCreateRequest;
 use App\User;
+use App\v1\Gadd as GaddOld;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Carbon\Carbon;
 
 class VonaController extends Controller
 {
-    /**
-     * Convert longitude and latitude to decimal text
-     *
-     * @return lat,lon string
-     */
-    protected function location($type,$decimal)
-    {
-        $vars = explode(".",$decimal);
-        $deg = $vars[0];
-        $tempma = "0.".$vars[1];
-        $tempma = $tempma * 3600;
-        $min = floor($tempma / 60);
-        $sec = round($tempma - ($min*60));
+    protected $latestVona;
 
-        $sym = "N ";
-        if ($deg<10 AND $deg>0){
-            $deg="0".$deg;
-        } else {
-            if ($deg<0){
-                $deg="0".abs($deg);
-                $sym="S ";
-            }
-        }
-        if ($min<10){
-            $min="0".$min;
-        }
-        if ($sec<10){
-            $sec="0".$sec;
-        }
+    protected $volcano;
 
-        if ($type == 'lat')
-        {
-            return $sym.$deg.' deg '.$min.' min '.$sec.' sec ';
-        }
-        return 'E '.$deg.' deg '.$min.' min '.$sec.' sec';
-    }
+    protected $location;
 
     /**
      * Display a listing of the resource.
@@ -106,62 +79,150 @@ class VonaController extends Controller
         return view('vona.create',compact('gadds','users'));
     }
 
+    protected function noticenumber(Request $request)
+    {
+        $year = now()->format('Y');
+        $code = $request->code;
+
+        $vonaCount = VonaOld::where('issued','like', '2022%')->where('ga_code', $code)->count();
+        $vonaCount = sprintf('%03d', $vonaCount);
+        return $year.$code.$vonaCount;
+    }
+
+    protected function datetimeUtc($datetime, $tz)
+    {
+        $datetime_utc = Carbon::createFromTimeString($datetime, $tz)->setTimezone('UTC');
+
+        return $datetime_utc;
+    }
+
+    protected function issued(Request $request)
+    {
+        switch ($this->volcano->ga_zonearea) {
+            case 'WIB':
+                $tz = 'Asia/Jakarta';
+                break;
+            case 'WITA':
+                $tz = 'Asia/Makassar';
+                break;
+            default:
+                $tz = 'Asia/Jayapura';
+                break;
+        }
+
+        return $this->datetimeUtc($request->date, $tz)->format('Y-m-d H:i:s');
+    }
+
+    protected function previous_code(Request $request)
+    {
+        if (is_null($this->latestVona))
+            return 'unassigned';
+
+        return $this->latestVona->pre_avcode;
+    }
+
+    protected function get_color(Request $request)
+    {
+        if ($request->height >= 6000)
+            return 'RED';
+
+        if ($request->height > 0)
+            return 'ORANGE';
+
+        return 'YELLOW';
+    }
+
+    protected function current_code(Request $request)
+    {
+        return $request->visibility ? $this->get_color($request) : 'ORANGE';
+    }
+
+    protected function convertLatitude()
+    {
+        $coordinate = $this->volcano->ga_lat_gapi;
+        [$degree, $decimal] = explode('.', $coordinate);
+        $symbol = $degree > 0 ? 'N' : 'S';
+        $decimal = abs($coordinate) - abs($degree);
+        $minute = floor($decimal * 60);
+        $second = round(($decimal*3600) - ($minute * 60));
+
+        $degree = sprintf('%02s', abs($degree));
+        $minute = sprintf('%02s', abs($minute));
+        $second = sprintf('%02s', abs($second));
+
+        return "$symbol $degree deg $minute min $second sec";
+    }
+
+    protected function convertLongitude()
+    {
+        $coordinate = $this->volcano->ga_lon_gapi;
+        [$degree, $decimal] = explode('.', $coordinate);
+
+        $decimal = abs($coordinate) - abs($degree);
+        $minute = floor($decimal * 60);
+        $second = round(($decimal * 3600) - ($minute * 60));
+
+        $degree = sprintf('%02s', abs($degree));
+        $minute = sprintf('%02s', abs($minute));
+        $second = sprintf('%02s', abs($second));
+
+        return "E $degree deg $minute min $second sec";
+    }
+
+    protected function location()
+    {
+        return "{$this->convertLatitude()} {$this->convertLongitude()}";
+    }
+
+    protected function volcano(Request $request)
+    {
+        $this->volcano = GaddOld::select('ga_code','ga_nama_gapi','ga_id_smithsonian','ga_elev_gapi','ga_lon_gapi','ga_lat_gapi','ga_prov_gapi', 'ga_prov_gapi_en','ga_zonearea')->where('ga_code', $request->code)->first();
+    }
+
+    protected function latestVona(Request $request)
+    {
+        $this->latestVona = VonaOld::with('volcano:ga_code,ga_nama_gapi,ga_id_smithsonian,ga_elev_gapi,ga_lon_gapi,ga_lat_gapi,ga_prov_gapi,ga_zonearea')
+            ->where('ga_code', $request->code)
+            ->where('sent', 1)
+            ->orderBy('issued_time', 'desc')
+            ->first();
+    }
+
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Http\Requests\VonaCreateRequest $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(VonaCreateRequest $request)
     {
-        $nip = $request->input('nip',auth()->user()->nip);
-        $type = $request->has('tipe') ? $request->tipe : 'real';
-
-        $request->merge([
-            'nip' => $nip,
-            'tipe' => $type
-        ]);
-
-        $this->validate($request, [
-            'nip' => 'required|digits_between:16,18',
-            'code' => 'required|size:3',
-            'tipe' => 'required|in:real,exercise',
-            'cucode' => 'required|in:green,yellow,orange,red',
-            'vas' => 'required|min:50',
-            'vch' => 'required|max:5',
-            'ovci' => 'required|min:20',
-            'remarks' => 'nullable'
-        ]);
-
-        $user = User::where('nip','like',$nip)->firstOrFail();
-        $gadd = Gadd::where('code',$request->code)->firstOrFail();
-
-        $location = $this->location('lat',$gadd->latitude).$this->location('lon',$gadd->longitude);
-
-        $issued = now('UTC')->toDateTimeString();
-        $tipe = strtoupper($request->input('tipe','real'));
-        $code = $request->code;
-        $cucode = strtoupper($request->cucode);
-        $vas = $request->vas;
-        $vch = $request->vch;
-        $ovci = $request->ovci;
-        $remarks = $request->remarks;
+        $this->latestVona($request);
+        $this->volcano($request);
 
         $vona = Vona::create([
-            'issued' => $issued,
-            'type' => $tipe,
-            'code_id' => $code,
-            'cu_code' => $cucode,
-            'location' => $location,
-            'vas' => $vas,
-            'vch_summit' => $vch,
-            'vch_asl' => $vch+$gadd->elevation,
-            'vch_other' => $ovci,
-            'remarks' => $remarks,
-            'nip_pelapor' => $nip
+            'noticenumber' => $this->noticenumber($request),
+            'issued' => $this->issued($request),
+            'type' => Str::upper($request->type),
+            'code_id' => $request->code,
+            'is_visible' => $request->visibility,
+            'is_continuing' => $request->erupsi_berlangsung,
+            'current_code' => $this->current_code($request),
+            'previous_code' => $this->previous_code($request),
+            'ash_height' => $request->height,
+            'ash_color' => $request->warna_asap,
+            'ash_intensity' => $request->intensitas,
+            'ash_directions' => $request->arah_abu,
+            'amplitude' => $request->amplitudo,
+            'duration' => $request->durasi ?? 0,
+            'nip_pelapor' => auth()->user()->nip,
         ]);
 
-        return view('vona.show',compact('vona'));
+        $vona->load('user','gunungapi');
+
+        return view('vona.show',[
+            'vona' => $vona,
+            'location' => $this->location()
+        ]);
 
     }
 
