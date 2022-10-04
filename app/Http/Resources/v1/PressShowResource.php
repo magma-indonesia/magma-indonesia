23c1fcc58212161494d8391347ae7b7dad4b714c
Martanto <martanto@live.com>
2022-10-04 13:02:49 +0700

    Press release

---
+++ app/Http/Resources/v1/PressShowResource.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Resources\v1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Str;
+
+class PressShowResource extends JsonResource
+{
+    protected function signedUrl()
+    {
+        return URL::signedRoute('v1.press.show', $this);
+    }
+
+    protected function datetTime()
+    {
+        $datetime = $this->datetime ?: $this->log;
+        return $datetime;
+    }
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'datetime' => $this->datetTime()->format('Y-m-d H:i:s'),
+            'datetime_text' => $this->datetTime()->format('Y-m-d H:i:s').' WIB',
+            'datetime_deskripsi' => $this->datetTime()->diffForHumans(),
+            'judul' => $this->judul,
+            'deskripsi' => strip_tags($this->deskripsi),
+            'image' => $this->fotolink,
+            'share' => [
+                'url' =>  $this->signedUrl(),
+                'description' => Str::limit(strip_tags($this->deskripsi), 280),
+                'photo' => $this->fotolink
+            ],
+        ];
+    }
+}
