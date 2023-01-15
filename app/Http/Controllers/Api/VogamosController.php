b589782aa089fce31519a5731bcb3a8c35e239b8
Martanto <martanto@live.com>
2023-01-15 21:19:29 +0700

    wip vogamos

--- app/Http/Controllers/Api/VogamosController.php
+++ app/Http/Controllers/Api/VogamosController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Vogamos\VogamosData;
 use App\Vogamos\VogamosStation;
+use Illuminate\Support\Collection;
 
 class VogamosController extends Controller
 {
@@ -14,12 +15,34 @@ class VogamosController extends Controller
         return [];
     }
 
+    protected function sortDateByDescThenGroupedByStation(Request $request): array
+    {
+        return collect($request->toArray())
+                ->sortByDesc('date_data')
+                ->groupBy('ID_stasiun')
+                ->all();
+    }
+
     public function store(Request $request)
     {
-        // $this->groupByStation->firstOrCreateStation->insertDataPerStation;
+        $stations = $this->sortDateByDescThenGroupedByStation($request);
 
-        $station = VogamosStation::where('ID_stasiun', $request->ID_stasiun)
-            ->firstOrFail();
+        [$stationsExists, $stationsNotExists] = collect($stations)->partition(function ($stationData, $stationId) {
+            return VogamosStation::where('ID_stasiun', $stationId)->exists();
+        });
+
+        return $stationsExists;
+
+        // $stationsNotExists = collect($stations)->keys()->reject(function ($station) {
+        //     return VogamosStation::where('ID_stasiun', $station)->exists();
+        // })->values();
+
+        // return $stationsNotExists;
+        // $this->groupByStation->firstOrCreateStation->insertDataPerStation->updateLastUpdate;
+
+        $station = VogamosStation::firstOrCreate([
+            'ID_stasiun' => strtoupper($request->ID_stasiun)
+        ]);
 
         $data = (new VogamosData())->setTable(strtoupper($request->ID_stasiun));
 
