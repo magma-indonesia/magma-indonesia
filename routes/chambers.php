7a34c69db40fffd665a45f7099ba4edf902d9b91
Martanto <martanto@live.com>
2023-03-09 21:35:46 +0700

    Update chambers.php

--- routes/chambers.php
+++ routes/chambers.php
@@ -23,7 +23,7 @@ Route::resource('pengajuan', PengajuanController::class);
 Route::get('overtime/{date?}/{flush?}', [OvertimeController::class, 'index'])
     ->name('overtime.index');
 Route::get('overtime/{nip}/show/{date?}', [OvertimeController::class, 'show'])
-    ->name('overtime.show.nip');
+    ->name('overtime.show-nip');
 // Route::resource('overtime', OvertimeController::class);
 
 Route::post('crs/lokasi','CrsController@getCities')->name('crs.getcities');
