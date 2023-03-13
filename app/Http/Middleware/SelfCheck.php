5f6639e66624d46ad9609e1f268c90a1c3b79ca5
Martanto <martanto@live.com>
2023-03-13 09:34:06 +0700

    Update SelfCheck.php

--- app/Http/Middleware/SelfCheck.php
+++ app/Http/Middleware/SelfCheck.php
@@ -20,12 +20,12 @@ class SelfCheck
                 ->where('id',$request->route('user'))
                 ->first();
 
-        if (auth()->user()->hasRole('Super Admin'))
+        if (request()->user()->hasRole('Super Admin'))
             return $next($request);
 
         if (auth()->user()->nip == $user->nip)
             return $next($request);
-            
+
         return redirect()->route('chambers.users.index');
     }
 }
