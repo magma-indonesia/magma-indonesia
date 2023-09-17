3c136b819e247ae9ab07651397fdb8d36521d331
Martanto <martanto@live.com>
2023-09-17 15:25:18 +0700

    add field 'code' to gadd

---
+++ database/migrations/2023_09_17_152327_add_code_to_old_gadd.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddCodeToOldGadd extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::connection('magma')->table('ga_dd', function (Blueprint $table) {
+            $table->char('code', 3)->unique();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('old_gadd', function (Blueprint $table) {
+            //
+        });
+    }
+}
